f44bd275a8f8bc4b6524d6dd741a5706abbc88ad
Guenter Roeck <linux@roeck-us.net>
2013-08-20 10:34:41 -0700

    Use full specified address to contact saturn
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/distribute.sh
+++ bin/distribute.sh
@@ -9,7 +9,7 @@ fi
 
 base=/opt/buildbot
 
-for system in saturn desktop jupiter
+for system in saturn.roeck-us.net desktop jupiter
 do
 	echo -n "${system}: "
 	rsync -r ${base}/bin ${base}/rootfs ${system}:${base}
