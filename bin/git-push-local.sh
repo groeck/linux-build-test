e922515c4fd943eb3c6fe9a6ecfb9b76dad6b01d
Guenter Roeck <linux@roeck-us.net>
2013-08-21 14:07:36 -0700

    git-push-local.sh: Catch errors
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/git-push-local.sh
+++ bin/git-push-local.sh
@@ -17,20 +17,26 @@ fi
 
 git fetch --all
 
-git branch -r | grep origin | grep -v HEAD | grep -v linux-2.6 | cut -f2 -d'/' | while read a
+for a in $(git branch -r | grep origin | grep -v HEAD | grep -v linux-2.6 | cut -f2 -d'/')
 do
 	echo -n "$a: "
 	git push local origin/$a:$a
 	if [ $? -ne 0 ]
 	then
-		echo "git push error: aborting"
+		echo "git push error (branches): aborting"
 		exit 1
 	fi
 done
 
-git tag -l | grep v3 | \
-	egrep -v "v3.1\$|v3.1\.|v3.1-|v3.3|v3.5|v3.6|v3.9|v3.8|v3.7|v3.2" | while read a
+for a in $(git tag -l | grep v3 | egrep -v "v3.1\$|v3.1\.|v3.1-|v3.3|v3.5|v3.6|v3.9|v3.8|v3.7|v3.2")
 do
 	echo -n "tag:$a: "
 	git push local refs/tags/$a
+	if [ $? -ne 0 ]
+	then
+		echo "git push error (tags): aborting"
+		exit 1
+	fi
 done
+
+exit 0
