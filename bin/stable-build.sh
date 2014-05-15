3959e82911a59feae9d6fdca8340b55bb6b69839
Guenter Roeck <linux@roeck-us.net>
2014-05-15 10:30:26 -0700

    Add powerpc build with binutils 2.24
    The assembler ABI in binutils 2.24 was changed, resulting in some build
    failures if binutils 2.24 is used.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/stable-build.sh
+++ bin/stable-build.sh
@@ -18,8 +18,8 @@ PATH_MICROBLAZE=/opt/kernel/gcc-4.8.0-nolibc/microblaze-linux/bin
 PATH_MIPS=/opt/poky/1.3/sysroots/x86_64-pokysdk-linux/usr/bin/mips32-poky-linux
 PATH_PARISC=/opt/kernel/gcc-4.6.3-nolibc/hppa-linux/bin
 PATH_PARISC64=/opt/kernel/gcc-4.6.3-nolibc/hppa64-linux/bin
-# PATH_PPC=/opt/poky/1.4.0-1/sysroots/x86_64-pokysdk-linux/usr/bin/ppc64e5500-poky-linux
 PATH_PPC=/opt/poky/1.5.1/sysroots/x86_64-pokysdk-linux/usr/bin/powerpc64-poky-linux
+PATH_PPC_24=/opt/poky/1.6/sysroots/x86_64-pokysdk-linux/usr/bin/powerpc64-poky-linux
 PATH_S390=/opt/kernel/gcc-4.6.3-nolibc/s390x-linux/bin
 PATH_SH4=/opt/kernel/gcc-4.6.3-nolibc/sh4-linux/bin
 PATH_SPARC=/opt/kernel/gcc-4.6.3-nolibc/sparc64-linux/bin
@@ -134,6 +134,12 @@ doit()
 	cmd=(${cmd_powerpc[*]})
 	PREFIX="powerpc64-poky-linux-"
 	PATH=${PATH_PPC}:${PATH}
+	;;
+    powerpc_24)
+	ARCH=powerpc
+	cmd=(${cmd_powerpc[*]})
+	PREFIX="powerpc64-poky-linux-"
+	PATH=${PATH_PPC_24}:${PATH}
 	;;
     sparc32)
 	cmd=(${cmd_sparc32[*]})
