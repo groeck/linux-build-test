3fce52c55d8d9761fe745aba3f3a95c587c56633
Guenter Roeck <linux@roeck-us.net>
2014-07-15 21:30:06 -0700

    Point to new version of xtensa toolchain
    Old toolchain tends to generate 'invalid jump target' linker error messages.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/stable-build.sh
+++ bin/stable-build.sh
@@ -26,7 +26,8 @@ PATH_SPARC=/opt/kernel/gcc-4.6.3-nolibc/sparc64-linux/bin
 PATH_TILE=/opt/kernel/gcc-4.6.2-nolibc/tilegx-linux/bin
 PATH_UC32=/opt/kernel/unicore32/uc4-1.0.5-hard/bin
 PATH_X86=/opt/poky/1.3/sysroots/x86_64-pokysdk-linux/usr/bin/x86_64-poky-linux
-PATH_XTENSA=/opt/kernel/gcc-4.6.3-nolibc/xtensa-linux/bin
+# PATH_XTENSA=/opt/kernel/gcc-4.6.3-nolibc/xtensa-linux/bin
+PATH_XTENSA=/opt/kernel/xtensa/gcc-4.7.3/usr/bin
 
 rel=$(git describe | cut -f1 -d- | cut -f1,2 -d.)
 
