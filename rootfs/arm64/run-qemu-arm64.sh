072fca33ee58b2a9308719cafe11a514bf6d4c75
Guenter Roeck <linux@roeck-us.net>
2014-08-12 01:09:47 -0700

    Add arm64 qemu target
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

---
+++ rootfs/arm64/run-qemu-arm64.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+
+PREFIX=aarch64-linux-
+ARCH=arm64
+rootfs=rootfs.arm64.cpio
+PATH_ARM64=/opt/kernel/gcc-4.8.1/aarch64-linux/bin
+
+logfile=/tmp/qemu.$$.log
+maxtime=120
+looptime=5
+
+PATH=${PATH_ARM64}:${PATH}
+
+dir=$(cd $(dirname $0); pwd)
+
+runkernel()
+{
+    local defconfig=$1
+    local pid
+    local retcode
+    local t
+
+    cp ${dir}/${defconfig} arch/${ARCH}/configs
+    make ARCH=${ARCH} CROSS_COMPILE=${PREFIX} ${defconfig}
+    if [ $? -ne 0 ]
+    then
+        echo "failed (config) - aborting"
+        return 1
+    fi
+
+    echo "Build reference: $(git describe)"
+    echo "Configuration file: ${defconfig}"
+    echo "Building kernel ..."
+    make -j12 ARCH=${ARCH} CROSS_COMPILE=${PREFIX} >${logfile} 2>&1
+    if [ $? -ne 0 ]
+    then
+	echo "Build failed - aborting"
+	echo "------------"
+	echo "Build log:"
+	cat ${logfile}
+	echo "------------"
+	rm -f ${logfile}
+	return 1
+    fi
+
+    echo -n "Running qemu ..."
+
+    rm -f ${logfile}
+
+    /opt/buildbot/bin/qemu-system-aarch64 -machine virt -cpu cortex-a57 \
+	-machine type=virt -nographic -smp 1 -m 2048 \
+	-kernel arch/arm64/boot/Image -initrd ${dir}/${rootfs} -no-reboot \
+	-append "console=ttyAMA0 doreboot" > ${logfile} 2>&1 &
+
+    pid=$!
+
+    retcode=0
+    t=0
+    while true
+    do
+	kill -0 ${pid} >/dev/null 2>&1
+	if [ $? -ne 0 ]
+	then
+		break
+	fi
+
+	# This qemu session doesn't stop by itself. We have to help it along.
+	grep "Restarting system" ${logfile} >/dev/null 2>&1
+	if [ $? -eq 0 ]
+	then
+		kill ${pid} >/dev/null 2>&1
+		break
+	fi
+
+	if [ $t -gt ${maxtime} ]
+	then
+		echo " timeout - aborting"
+		kill ${pid} >/dev/null 2>&1
+		# give it some time to die, then kill it
+		# the hard way hard if it did not work.
+		sleep 5
+		kill -0 ${pid} >/dev/null 2>&1
+		if [ $? -eq 0 ]
+		then
+			kill -9 ${pid} >/dev/null 2>&1
+		fi
+		retcode=1
+		break
+	fi
+	sleep ${looptime}
+	t=$(($t + ${looptime}))
+	echo -n .
+    done
+
+    echo
+    grep "Boot successful" ${logfile} >/dev/null 2>&1
+    if [ ${retcode} -eq 0 -a $? -ne 0 ]
+    then
+	echo "No 'Boot successful' message in log. Test failed."
+	retcode=1
+    fi
+
+    grep "Rebooting" ${logfile} >/dev/null 2>&1
+    if [ ${retcode} -eq 0 -a $? -ne 0 ]
+    then
+	echo "No 'Rebooting' message in log. Test failed."
+	retcode=1
+    fi
+
+    grep "Restarting" ${logfile} >/dev/null 2>&1
+    if [ ${retcode} -eq 0 -a $? -ne 0 ]
+    then
+	echo "No 'Restarting' message in log. Test failed."
+	retcode=1
+    fi
+
+    dolog=0
+    grep "\[ cut here \]" ${logfile} >/dev/null 2>&1
+    if [ $? -eq 0 ]
+    then
+	dolog=1
+    fi
+
+    if [ ${retcode} -ne 0 -o ${dolog} -ne 0 ]
+    then
+	echo "------------"
+	echo "qemu log:"
+	cat ${logfile}
+	echo "------------"
+    else
+	echo "Test successful"
+    fi
+
+    return ${retcode}
+}
+
+runkernel qemu_arm64_smp_defconfig
+retcode=$?
+runkernel qemu_arm64_nosmp_defconfig
+retcode=$((${retcode} + $?))
+
+git clean -d -x -f -q
+
+rm -f ${logfile}
+exit ${retcode}
