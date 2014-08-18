f2bf7a38f6ef73e322adf272e1be79110f9d7812
Guenter Roeck <linux@roeck-us.net>
2014-08-18 15:22:05 -0700

    hwmon-build: Improved build tests
    Use KCONFIG_ALLCONFIG for improved coverage and faster builds.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/hwmon-build.sh
+++ bin/hwmon-build.sh
@@ -7,14 +7,19 @@ ERR=/tmp/error.$$
 BRANCH=$(git branch | egrep "^\*" | cut -f2 -d' ')
 
 PATH_X86=/opt/poky/1.3/sysroots/x86_64-pokysdk-linux/usr/bin/x86_64-poky-linux
+PATH_ARM=/opt/poky/1.3/sysroots/x86_64-pokysdk-linux/usr/bin/armv5te-poky-linux-gnueabi
+PATH_PPC=/opt/poky/1.5.1/sysroots/x86_64-pokysdk-linux/usr/bin/powerpc64-poky-linux
 
-export PATH=${PATH_X86}:${PATH}
+export PATH=${PATH_X86}:${PATH_ARM}:${PATH_PPC}:${PATH}
 
 errors=0
 builds=0
-retcode=0
+seq=0
 
 maxload=$(($(nproc) + 4))
+
+bindir=$(cd $(dirname $0); pwd)
+kdir=$(dirname ${bindir})/kconfig
 
 send_mail()
 {
@@ -31,143 +36,207 @@ send_mail()
 	rm -f ${MAIL_FILE}
 }
 
+doclean()
+{
+	local ARCH=$1
+	local noclean
+
+	pwd | grep buildbot >/dev/null 2>&1
+	noclean=$?
+
+	if [ ${noclean} -eq 0 ]
+	then
+		git clean -x -d -f -q
+	else
+		make ARCH=${ARCH} mrproper >/dev/null 2>&1
+		rm -f kconfig
+	fi
+}
+
+genconfig()
+{
+    local overlay=$1
+    local CROSS=$2
+    local ARCH=$3
+    local what=$4
+    local retval
+
+    cp ${kdir}/kconfig.common kconfig
+    cat ${kdir}/${overlay} >> kconfig
+
+    if [ "${what}" = "m" ]
+    then
+	echo "CONFIG_MODULES=y" >> kconfig
+	(cd drivers; \
+	    grep "config SENSORS" $(find watchdog hwmon -name Kconfig) | \
+		awk '{ printf "CONFIG_%s=m\n",$2; }') >> kconfig
+    else
+	# Add all sensors and watchdog drivers
+	(cd drivers; \
+	    grep "config SENSORS" $(find watchdog hwmon -name Kconfig) | \
+		awk '{ printf "CONFIG_%s=y\n",$2; }') >> kconfig
+    fi
+
+    KCONFIG_ALLCONFIG=kconfig make ${CROSS} ARCH=${ARCH} allnoconfig >/dev/null 2>&1
+    retval=$?
+    # fix fallout, if any
+    make ${CROSS} ARCH=${ARCH} oldnoconfig >/dev/null 2>&1
+
+    return ${retval}
+}
+
 doit()
 {
     local ARCH=$1
-    local i
+    local overlays=$(cd ${kdir}; ls kconfig.hwmon*)
     local files
+    local sfiles
+    local failed=0
 
     # We should be on the target directory, in the target branch
 
-    local OPTIONS=""
-    local SMATCH=0
-
-    PREFIX="x86_64-poky-linux-"
-
     case ${ARCH} in
     x86_64)
-	OPTIONS="W=1 C=1"
-	SMATCH=1
+	PREFIX="x86_64-poky-linux-"
+	;;
+    i386)
+	PREFIX="x86_64-poky-linux-"
+	;;
+    arm)
+	PREFIX="arm-poky-linux-gnueabi-"
+	;;
+    powerpc)
+	PREFIX="powerpc64-poky-linux-"
+	;;
+    *)
+	return 1
 	;;
     esac
 
-    if [ "${PREFIX}" != "" ]; then
-	CROSS="CROSS_COMPILE=${PREFIX}"
+    CROSS="CROSS_COMPILE=${PREFIX}"
+
+    rm -f ${ERR}
+    touch ${ERR}
+    doclean ${ARCH}
+    for overlay in ${overlays}
+    do
+	echo "$(basename $0): build:${overlay} branch:${BRANCH} arch:${ARCH} prefix:${PREFIX}"
+
+	genconfig ${overlay} ${CROSS} ${ARCH} m
+	if [ $? -ne 0 ]
+	then
+		echo failed to configure noconfig:${overlay}:m for ${ARCH}
+		continue
+	fi
+
+	builds=$(expr ${builds} + 1)
+	# Run with warnings disabled to reduce noise.
+	make ${CROSS} -j${maxload} -i ARCH=${ARCH} >/dev/null 2> >(tee ${ERR}.tmp >&2)
+	failed=$(($? + ${failed}))
+
+	cat ${ERR}.tmp >> ${ERR}
+
+	# Repeat, this time build everything into kernel
+
+	# doclean ${ARCH}
+	genconfig ${overlay} ${CROSS} ${ARCH} y
+	if [ $? -ne 0 ]
+	then
+		echo failed to configure noconfig:${overlay}:y for ${ARCH}
+		continue
+	fi
+
+	builds=$(expr ${builds} + 1)
+	make ${CROSS} -j${maxload} -i ARCH=${ARCH} >/dev/null 2> >(tee ${ERR}.tmp >&2)
+	failed=$((${failed} + $?))
+	cat ${ERR}.tmp >> ${ERR}
+	rm -f ${ERR}.tmp
+    done
+
+    if [ ${failed} -ne 0 ]
+    then
+	errors=$(expr ${errors} + 1)
+	echo "$(basename $0): branch:${BRANCH} arch:${ARCH} prefix:${PREFIX} failed"
+
+	[ -s ${WARN} ] && {
+	    echo "--------------------" >> ${WARN}
+	}
+	echo "Build: ${BRANCH}:${ARCH}" >> ${WARN}
+	echo >> ${WARN}
+	cat ${ERR} >> ${WARN}
+	echo "--------------------" >> ${WARN}
+	echo "Build: ${BRANCH}:${ARCH} failed"
+	return 1
     fi
 
-    i=0
-    while [ $i -lt 2 ]
-    do
-	failed=0
-	echo "$(basename $0): build:$i branch:${BRANCH} arch:${ARCH} prefix:${PREFIX}"
-	git clean -x -d -f -q
-	make ${CROSS} ARCH=${ARCH} defconfig >/dev/null 2>&1
-	if [ $? -ne 0 ]
-	then
-		echo failed to run defconfig for ${ARCH}:$i
-	 	i=$(expr $i + 1)
-	 	continue
-	fi
-	# Fix up configuration file.
-	# First run, build as modules. Second run, build into kernel.
-	if [ $i -eq 0 ]
-	then
-	    sed -i 's/^# \(CONFIG_SENSORS_\)\([A-Za-z0-9_]*\).*/\1\2=m/g' .config
-	else
-	    sed -i 's/^# \(CONFIG_SENSORS_\)\([A-Za-z0-9_]*\).*/\1\2=y/g' .config
-	fi
-	make ${CROSS} ARCH=${ARCH} oldnoconfig >/dev/null 2>&1
-	if [ $? -ne 0 ]
-	then
-		echo failed to run oldnoconfig for ${ARCH}:$i
-	 	i=$(expr $i + 1)
-	 	continue
-	fi
-	builds=$(expr ${builds} + 1)
-	make ${CROSS} -j${maxload} -i ARCH=${ARCH} >/dev/null 2> >(tee ${ERR} >&2)
-	failed=$?
-	#
-	# If options are set, repeat the exercise for all object files
-	# in drivers/hwmon and drivers/watchdog. Only do it for the
-	# module build; this should catch most buildable sources.
-	# This reduces build time and limits the number of repetitive warnings
-	# we have to deal with.
-	# The odd redirect is to get error output to the console (for the
-	# buildbot log) and into a file for the status email.
-	#
-	rm -f ${WARN}.sparse
-	touch ${WARN}.sparse
-	rm -f ${WARN}.smatch
-	touch ${WARN}.smatch
-	if [ -n "${OPTIONS}" -a $i -eq 0 ]
-	then
-	    files=$(find drivers/hwmon drivers/watchdog -name '*.o' |
+    # Check for warnings / errors only during first run
+    if [ ${seq} -ne 0 ]
+    then
+	rm -f ${ERR}
+        return 0
+    fi
+
+    seq=$((${seq} + 1))
+
+    # For remaining tests use object files generated by last build
+
+    # Repeat with W=1 C=1, but only for object files in 
+    # drivers/hwmon and drivers/watchdog to reduce noise.
+    # The odd redirect is to get error output to the console (for the
+    # buildbot log) and into a file for the status email.
+
+    files=$(find drivers/hwmon drivers/watchdog -name '*.o' |
 			grep -v built-in.o | egrep -v 'mod.o$' |
 			grep -v watchdog.o 2>/dev/null)
-	    rm -f ${files}
-	    make ${CROSS} -j${maxload} -i ${OPTIONS} ARCH=${ARCH} ${files} >/dev/null 2> >(tee ${ERR}.tmp >&2)
-	    egrep '^drivers/(hwmon|watchdog)' ${ERR}.tmp > ${WARN}.sparse 2>&1
-	    rm -f ${ERR}.tmp
-	    # If smatch build is asked for as well, do another run with smatch.
-	    # Append smatch log messages to warning log.
-	    # Run smatch on all sources, and ignore errors from the build step.
-	    if [ ${SMATCH} -ne 0 ]
-	    then
-		sfiles=$(find drivers/hwmon drivers/watchdog -name '*.c' |
+    rm -f ${files}
+    make ${CROSS} -j${maxload} -i W=1 C=1 ARCH=${ARCH} ${files} >/dev/null 2> >(tee ${ERR}.tmp >&2)
+    egrep '^drivers/(hwmon|watchdog)' ${ERR}.tmp > ${WARN}.sparse 2>&1
+    rm -f ${ERR}.tmp
+
+    # Finally do another run with smatch.
+    # Append smatch log messages to warning log.
+    # Run smatch on all sources, and ignore errors from the build step.
+    sfiles=$(find drivers/hwmon drivers/watchdog -name '*.c' |
 			grep -v built-in.c |
 			egrep -v '.mod.c$' | sed -e 's/\.c/.o/')
-		rm -f ${sfiles}
-		make C=1 -j${maxload} -i \
+    rm -f ${sfiles} ${files}
+    make C=1 -j${maxload} -i \
 		    CHECK="/opt/buildbot/bin/smatch --project=kernel" \
 		    ${sfiles} 2>/dev/null | tee ${WARN}.smatch.tmp
-		egrep '(warn|error|info):' ${WARN}.smatch.tmp | \
+    egrep '(warn|error|info):' ${WARN}.smatch.tmp | \
 				egrep -v "atomic\.h:.*ignoring unreachable code" | \
 				egrep -v "bitops\.h:.*ignoring unreachable code" \
-				>> ${WARN}.smatch
-		rm -f ${WARN}.smatch.tmp
-	    fi
-	fi
-	if [ ${failed} -ne 0 ]; then
-	    errors=$(expr ${errors} + 1)
-	    echo "$(basename $0): build:$i branch:${BRANCH} arch:${ARCH} prefix:${PREFIX} failed"
-	fi
-	# egrep "drivers/{hwmon|watchdog}" ${ERR} > ${WARN}.tmp 2>&1
-	if [ ${failed} -ne 0 -o -s ${WARN}.smatch -o -s ${WARN}.sparse ]; then
-	    [ -s ${WARN} ] && {
+				> ${WARN}.smatch
+    rm -f ${WARN}.smatch.tmp
+    # egrep "drivers/{hwmon|watchdog}" ${ERR} > ${WARN}.tmp 2>&1
+    if [ -s ${WARN}.smatch -o -s ${WARN}.sparse ]; then
+	[ -s ${WARN} ] && {
 		echo "--------------------" >> ${WARN}
-	    }
-	    echo "Build: ${BRANCH}:${ARCH}:$i" >> ${WARN}
-	    echo >> ${WARN}
-	    [ ${failed} -ne 0 ] && {
-		cat ${ERR} >> ${WARN}
-		echo "--------------------" >> ${WARN}
-	    }
-	    [ -s ${WARN}.sparse ] && {
+	}
+	echo "Build: ${BRANCH}:${ARCH}" >> ${WARN}
+	echo >> ${WARN}
+	[ -s ${WARN}.sparse ] && {
 		echo "sparse log:" >> ${WARN}
 	        cat ${WARN}.sparse >> ${WARN}
 		echo "--------------------" >> ${WARN}
-	    }
-	    [ -s ${WARN}.smatch ] && {
+	}
+	[ -s ${WARN}.smatch ] && {
 		echo "smatch log:" >> ${WARN}
 	        cat ${WARN}.smatch >> ${WARN}
-	    }
-	    [ ${failed} -ne 0 ] && {
-		echo "Build: ${BRANCH}:${ARCH}:$i failed with build errors"
-		retcode=1
-	    }
-	fi
-	rm -f ${WARN}.tmp ${ERR} ${WARN}.smatch
-	i=$(expr $i + 1)
-    done
+	}
+    fi
+    rm -f ${WARN}.tmp ${ERR} ${WARN}.smatch
+
     return 0
 }
 
-for build in x86_64 i386
+retcode=0
+for x in x86_64 i386 arm powerpc
 do
-	doit ${build}
+    doit $x
+    retcode=$(($? + ${retcode}))
+    doclean $x
 done
-
-git clean -d -x -f -q
 
 echo >> ${WARN}
 echo "-----------------------" >> ${WARN}
