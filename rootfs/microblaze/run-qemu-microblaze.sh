d92935a25526fede305977270ad8e1aaca96debf
Guenter Roeck <linux@roeck-us.net>
2014-08-27 13:11:29 -0700

    rootfs: run-qemu-microblaze: Modularize
    Make the script easier to maintain, and support executing it
    from an arbitrary linux tree.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/microblaze/run-qemu-microblaze.sh
+++ rootfs/microblaze/run-qemu-microblaze.sh
@@ -3,7 +3,6 @@
 PREFIX=microblaze-linux-
 ARCH=microblaze
 rootfs=rootfs.cpio
-defconfig=qemu_microblaze_defconfig
 PATH_MICROBLAZE=/opt/kernel/gcc-4.8.0-nolibc/microblaze-linux/bin
 
 logfile=/tmp/qemu.$$.log
@@ -14,44 +13,66 @@ PATH=${PATH_MICROBLAZE}:${PATH}
 
 dir=$(cd $(dirname $0); pwd)
 
-git clean -d -x -f -q
+doclean()
+{
+	pwd | grep buildbot >/dev/null 2>&1
+	if [ $? -eq 0 ]
+	then
+		git clean -x -d -f -q
+	else
+		make ARCH=${ARCH} mrproper >/dev/null 2>&1
+	fi
+}
 
-cp ${dir}/${defconfig} arch/${ARCH}/configs
-make ARCH=${ARCH} CROSS_COMPILE=${PREFIX} ${defconfig} >/dev/null
-if [ $? -ne 0 ]
-then
-    echo "failed (config) - aborting"
-    exit 1
-fi
+runkernel()
+{
+    local defconfig=$1
+    local pid
+    local retcode
+    local t
 
-cp ${dir}/${rootfs} .
+    doclean
 
-echo "Build reference: $(git describe)"
-echo "Building kernel ..."
-make -j12 ARCH=${ARCH} CROSS_COMPILE=${PREFIX} >${logfile} 2>&1
-if [ $? -ne 0 ]
-then
+    cp ${dir}/${defconfig} arch/${ARCH}/configs
+    make ARCH=${ARCH} CROSS_COMPILE=${PREFIX} ${defconfig} >/dev/null
+    if [ $? -ne 0 ]
+    then
+        echo "failed (config) - aborting"
+        return 1
+    fi
+
+    cp ${dir}/${rootfs} .
+
+    echo "Build reference: $(git describe)"
+    echo "Configuration file: ${defconfig}"
+    echo "Building kernel ..."
+    make -j12 ARCH=${ARCH} CROSS_COMPILE=${PREFIX} >${logfile} 2>&1
+    if [ $? -ne 0 ]
+    then
 	echo "Build failed - aborting"
 	echo "------------"
 	echo "Build log:"
 	cat ${logfile}
 	echo "------------"
 	rm -f ${logfile}
-	exit 1
-fi
+	return 1
+    fi
 
-echo -n "Running qemu ..."
+    echo -n "Running qemu ..."
 
-rm -f ${logfile}
+    rm -f ${logfile}
 
-/opt/buildbot/bin/qemu-system-microblaze -M petalogix-s3adsp1800 -kernel arch/microblaze/boot/linux.bin -no-reboot -append "console=ttyUL0,115200 doreboot" -nographic > ${logfile} 2>&1 &
+    /opt/buildbot/bin/qemu-system-microblaze -M petalogix-s3adsp1800 \
+	-kernel arch/microblaze/boot/linux.bin -no-reboot \
+	-append "console=ttyUL0,115200 doreboot" -nographic \
+	> ${logfile} 2>&1 &
 
-pid=$!
+    pid=$!
 
-retcode=0
-t=0
-while true
-do
+    retcode=0
+    t=0
+    while true
+    do
 	kill -0 ${pid} >/dev/null 2>&1
 	if [ $? -ne 0 ]
 	then
@@ -83,46 +104,51 @@ do
 	sleep ${looptime}
 	t=$(($t + ${looptime}))
 	echo -n .
-done
+    done
 
-echo
-grep "Boot successful" ${logfile} >/dev/null 2>&1
-if [ ${retcode} -eq 0 -a $? -ne 0 ]
-then
+    echo
+    grep "Boot successful" ${logfile} >/dev/null 2>&1
+    if [ ${retcode} -eq 0 -a $? -ne 0 ]
+    then
 	echo "No 'Boot successful' message in log. Test failed."
 	retcode=1
-fi
+    fi
 
-grep "Rebooting" ${logfile} >/dev/null 2>&1
-if [ ${retcode} -eq 0 -a $? -ne 0 ]
-then
+    grep "Rebooting" ${logfile} >/dev/null 2>&1
+    if [ ${retcode} -eq 0 -a $? -ne 0 ]
+    then
 	echo "No 'Rebooting' message in log. Test failed."
 	retcode=1
-fi
+    fi
 
-grep "Machine restart" ${logfile} >/dev/null 2>&1
-if [ ${retcode} -eq 0 -a $? -ne 0 ]
-then
+    grep "Machine restart" ${logfile} >/dev/null 2>&1
+    if [ ${retcode} -eq 0 -a $? -ne 0 ]
+    then
 	echo "No 'Machine restart' message in log. Test failed."
 	retcode=1
-fi
+    fi
 
-dolog=0
-grep "\[ cut here \]" ${logfile} >/dev/null 2>&1
-if [ $? -eq 0 ]
-then
+    dolog=0
+    grep "\[ cut here \]" ${logfile} >/dev/null 2>&1
+    if [ $? -eq 0 ]
+    then
 	dolog=1
-fi
+    fi
 
-if [ ${retcode} -ne 0 -o ${dolog} -ne 0 ]
-then
+    if [ ${retcode} -ne 0 -o ${dolog} -ne 0 ]
+    then
 	echo "------------"
 	echo "qemu log:"
 	cat ${logfile}
 	echo "------------"
-else
+    else
 	echo "Test successful"
-fi
+    fi
+    return ${retcode}
+}
+
+runkernel qemu_microblaze_defconfig
+retcode=$?
 
 rm -f ${logfile}
 exit ${retcode}
