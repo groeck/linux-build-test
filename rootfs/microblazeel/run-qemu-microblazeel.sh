ac73c028ee71775f7e3ac6e9e87612e9605854a2
Guenter Roeck <linux@roeck-us.net>
2014-10-03 17:32:09 -0700

    rootfs: Add microblazeel (Microblaze little endian)
    Similar to microblaze. New root file system built with Grant's prototype.
    Few different build options were required, and it needs a little endian compiler
    which can be built using buildroot.
    
    Also added poweroff branch to build targets.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

---
+++ rootfs/microblazeel/run-qemu-microblazeel.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+PREFIX=microblazeel-linux-
+ARCH=microblaze
+rootfs=busybox-microblazeel.cpio
+PATH_MICROBLAZE=/opt/kernel/microblazeel/gcc-4.9.1/usr/bin
+
+PATH=${PATH_MICROBLAZE}:${PATH}
+
+dir=$(cd $(dirname $0); pwd)
+
+. ${dir}/../scripts/common.sh
+
+runkernel()
+{
+    local defconfig=$1
+    local pid
+    local retcode
+    local waitlist=("Machine restart" "Boot successful" "Rebooting")
+    local logfile=/tmp/runkernel-$$.log
+
+    echo -n "Building ${ARCH}:${defconfig} ... "
+
+    dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig}
+    if [ $? -ne 0 ]
+    then
+	return 1
+    fi
+
+    echo -n "running ..."
+
+    /opt/buildbot/bin/qemu-system-microblazeel -M petalogix-s3adsp1800 \
+	-kernel arch/microblaze/boot/linux.bin -no-reboot -nographic \
+	> ${logfile} 2>&1 &
+
+    pid=$!
+
+    dowait ${pid} ${logfile} manual waitlist[@]
+    retcode=$?
+    rm -f ${logfile}
+    return ${retcode}
+}
+
+echo "Build reference: $(git describe)"
+echo
+
+runkernel qemu_microblazeel_defconfig
+
+exit $?
