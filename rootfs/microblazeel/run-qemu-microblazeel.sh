772d535d4a36d57efe137701b9f9667eb8574798
Guenter Roeck <linux@roeck-us.net>
2015-02-16 12:45:07 -0800

    rootfs: microblazeel: Convert to dynamic runtime code
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/microblazeel/run-qemu-microblazeel.sh
+++ rootfs/microblazeel/run-qemu-microblazeel.sh
@@ -21,7 +21,7 @@ runkernel()
 
     echo -n "Building ${ARCH}:${defconfig} ... "
 
-    dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig}
+    dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig} dynamic
     if [ $? -ne 0 ]
     then
 	return 1
