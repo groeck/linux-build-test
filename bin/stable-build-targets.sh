35c162671ea2c7d8e267f32e489f685307544ec0
Guenter Roeck <linux@roeck-us.net>
2015-06-01 07:13:27 -0700

    Drop obsolete build targets
    Drop build targets which have been removed from recent kernels.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/stable-build-targets.sh
+++ bin/stable-build-targets.sh
@@ -14,10 +14,8 @@ cmd_arm=(allmodconfig s3c2410_defconfig omap2plus_defconfig imx_v6_v7_defconfig 
 	s3c6400_defconfig lpc32xx_defconfig shmobile_defconfig \
 	nhk8815_defconfig bcm2835_defconfig sama5_defconfig orion5x_defconfig \
 	exynos_defconfig cm_x2xx_defconfig s5pv210_defconfig \
-	integrator_defconfig msm_defconfig kirkwood_defconfig \
-	at91rm9200_defconfig s5p64x0_defconfig efm32_defconfig \
-	pxa910_defconfig clps711x_defconfig s5pc100_defconfig \
-	exynos4_defconfig ap4evb_defconfig bonito_defconfig mvebu_defconfig)
+	integrator_defconfig efm32_defconfig \
+	pxa910_defconfig clps711x_defconfig)
 cmd_arm64=(defconfig allmodconfig)
 cmd_avr32=(defconfig merisc_defconfig atngw100mkii_evklcd101_defconfig)
 cmd_blackfin=(defconfig BF561-EZKIT-SMP_defconfig)
@@ -25,6 +23,7 @@ cmd_c6x=(dsk6455_defconfig evmc6457_defconfig evmc6678_defconfig)
 cmd_cris=(defconfig etrax-100lx_defconfig allnoconfig)
 cmd_crisv32=(artpec_3_defconfig etraxfs_defconfig)
 cmd_frv=(defconfig)
+cmd_h8300=(edosk2674_defconfig h8300h-sim_defconfig h8s-sim_defconfig)
 cmd_hexagon=(defconfig)
 cmd_i386=(defconfig allyesconfig allmodconfig allnoconfig)
 cmd_ia64=(defconfig)
@@ -36,14 +35,14 @@ cmd_metag=(defconfig meta1_defconfig meta2_defconfig meta2_smp_defconfig)
 cmd_microblaze=(mmu_defconfig nommu_defconfig)
 cmd_mips=(defconfig allmodconfig bcm47xx_defconfig bcm63xx_defconfig \
 	nlm_xlp_defconfig ath79_defconfig ar7_defconfig fuloong2e_defconfig \
-	e55_defconfig cavium_octeon_defconfig powertv_defconfig malta_defconfig)
+	e55_defconfig cavium_octeon_defconfig malta_defconfig)
 cmd_mn10300=(asb2303_defconfig asb2364_defconfig)
 cmd_nios2=(3c120_defconfig)
 cmd_openrisc=(defconfig)
 cmd_parisc=(defconfig generic-32bit_defconfig)
 cmd_parisc64=(a500_defconfig generic-64bit_defconfig)
 cmd_powerpc=(defconfig allmodconfig ppc64e_defconfig cell_defconfig \
-	chroma_defconfig maple_defconfig ppc6xx_defconfig mpc83xx_defconfig \
+	maple_defconfig ppc6xx_defconfig mpc83xx_defconfig \
 	mpc85xx_defconfig mpc85xx_smp_defconfig tqm8xx_defconfig \
 	85xx/sbc8548_defconfig 83xx/mpc834x_mds_defconfig \
 	86xx/sbc8641d_defconfig)
