4ea60988511e110739b9d8565dd08cd709dbb1d2
Guenter Roeck <linux@roeck-us.net>
2015-08-02 08:37:02 -0700

    rootfs: ppc: Add runtime test for Virtex 440
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/ppc/run-qemu-ppc.sh
+++ rootfs/ppc/run-qemu-ppc.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # machine specific information
-rootfs=core-image-minimal-qemuppc.ext3
 # PATH_PPC=/opt/poky/1.4.0-1/sysroots/x86_64-pokysdk-linux/usr/bin/ppc64e5500-poky-linux
 PATH_PPC=/opt/poky/1.5.1/sysroots/x86_64-pokysdk-linux/usr/bin/powerpc64-poky-linux
 PATH_X86=/opt/poky/1.4.0-1/sysroots/x86_64-pokysdk-linux/usr/bin
@@ -19,6 +18,9 @@ dir=$(cd $(dirname $0); pwd)
 runkernel()
 {
     local defconfig=$1
+    local mach=$2
+    local rootfs=$3
+    local dtb=$4
     local pid
     local retcode
     local logfile=/tmp/runkernel-$$.log
@@ -34,10 +36,17 @@ runkernel()
 
     echo -n "running ..."
 
-    ${QEMUCMD} -kernel ${KERNEL_IMAGE} -M ${QEMU_MACH} -cpu G4 \
+    if [ "${mach}" = "mac99" ]
+    then
+      ${QEMUCMD} -kernel ${KERNEL_IMAGE} -M ${mach} -cpu G4 \
 	-hda ${rootfs} -usb -usbdevice wacom-tablet -no-reboot -m 128 \
 	--append "root=/dev/hda rw mem=128M console=ttyS0 console=tty doreboot" \
 	-nographic > ${logfile} 2>&1 &
+    else
+      ${QEMUCMD} -kernel ${KERNEL_IMAGE} -M ${mach} -no-reboot -m 256 \
+        --append "rdinit=/sbin/init console=ttyS0 console=tty doreboot" \
+	-dtb ${dtb} -nographic > ${logfile} 2>&1 &
+    fi
 
     pid=$!
 
@@ -50,9 +59,17 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel qemu_ppc_book3s_defconfig
+VIRTEX_DTB=arch/powerpc/boot/dts/virtex440-ml507.dtb
+if [ -e arch/powerpc/boot/dts/virtex440-ml507.dts ]
+then
+    dtc -I dts -O dtb arch/powerpc/boot/dts/virtex440-ml507.dts -o ${VIRTEX_DTB} >/dev/null 2>&1
+fi
+
+runkernel qemu_ppc_book3s_defconfig mac99 core-image-minimal-qemuppc.ext3
 retcode=$?
-runkernel qemu_ppc_book3s_smp_defconfig
+runkernel qemu_ppc_book3s_smp_defconfig mac99 core-image-minimal-qemuppc.ext3
+retcode=$((${retcode} + $?))
+runkernel qemu_virtex440_defconfig virtex-ml507 busybox-ppc.cpio ${VIRTEX_DTB}
 retcode=$((${retcode} + $?))
 
 exit ${retcode}
