ac71ecff70fde99e9c5212956e4e8419c7a88e00
Guenter Roeck <linux@roeck-us.net>
2015-08-07 13:34:59 -0700

    rootfs: sparc64: Use default configurations
    Using the default configuration as starting point lets us run
    sparc64 qemu tests on 3.2 and 3.4 kernels.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/sparc64/run-qemu-sparc64.sh
+++ rootfs/sparc64/run-qemu-sparc64.sh
@@ -13,26 +13,56 @@ dir=$(cd $(dirname $0); pwd)
 
 cached_defconfig=""
 
+patch_defconfig()
+{
+    local defconfig=$1
+    local smp=$2
+
+    # Configure SMP as requested, enable DEVTMPFS, and enable VIRTIO
+
+    sed -i -e '/CONFIG_SMP/d' ${defconfig}
+    sed -i -e '/CONFIG_DEVTMPFS/d' ${defconfig}
+    sed -i -e '/VIRTIO/d' ${defconfig}
+    echo "
+CONFIG_DEVTMPFS=y
+CONFIG_VIRTIO=y
+CONFIG_VIRTIO_PCI=y
+CONFIG_VIRTIO_BLK=y
+CONFIG_VIRTIO_NET=y
+CONFIG_VIRTIO_BALLOON=y
+CONFIG_VIRTIO_CONSOLE=y
+CONFIG_SCSI_VIRTIO=y
+    " >> ${defconfig}
+
+    if [ "${smp}" = "nosmp" ]
+    then
+	echo "# CONFIG_SMP is not set" >> ${defconfig}
+    else
+	echo "CONFIG_SMP=y" >> ${defconfig}
+    fi
+}
+
 runkernel()
 {
     local defconfig=$1
     local machine=$2
     local cpu="$3"
+    local smp=$4
     local pid
     local retcode
     local logfile=/tmp/runkernel-$$.log
     local waitlist=("Restarting system" "Boot successful" "Rebooting")
 
-    echo -n "Building ${ARCH}:${machine}:${defconfig} ... "
+    echo -n "Building ${ARCH}:${machine}:${smp}:${defconfig} ... "
 
-    if [ "${defconfig}" != "${cached_defconfig}" ]
+    if [ "${defconfig}_${smp}" != "${cached_defconfig}" ]
     then
-	dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig}
+	dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig} "" ${smp}
 	if [ $? -ne 0 ]
 	then
 	    return 1
 	fi
-	cached_defconfig=${defconfig}
+	cached_defconfig="${defconfig}_${smp}"
     fi
 
     echo -n "running ..."
@@ -55,13 +85,13 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel qemu_sparc_smp_defconfig sun4u "TI UltraSparc IIi"
+runkernel sparc64_defconfig sun4u "TI UltraSparc IIi" smp
 retcode=$?
-runkernel qemu_sparc_smp_defconfig sun4v "Fujitsu Sparc64 IV"
+runkernel sparc64_defconfig sun4v "Fujitsu Sparc64 IV" smp
 retcode=$((${retcode} + $?))
-runkernel qemu_sparc_nosmp_defconfig sun4u "TI UltraSparc IIi"
+runkernel sparc64_defconfig sun4u "TI UltraSparc IIi" nosmp
 retcode=$((${retcode} + $?))
-runkernel qemu_sparc_nosmp_defconfig sun4v "Fujitsu Sparc64 IV"
+runkernel sparc64_defconfig sun4v "Fujitsu Sparc64 IV" nosmp
 retcode=$((${retcode} + $?))
 
 exit ${retcode}
