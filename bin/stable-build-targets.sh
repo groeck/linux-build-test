9d4fcb7064306ff763fa666a7f036f4dbc1db1e6
Guenter Roeck <linux@roeck-us.net>
2015-08-09 21:47:25 -0700

    stable-build-targets: Drop duplicate builds
    Various builds are now done as qemu builds. Building those again
    as build targets is redundant, so drop them to save some time.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/stable-build-targets.sh
+++ bin/stable-build-targets.sh
@@ -8,7 +8,7 @@ cmd_alpha=(defconfig allmodconfig allnoconfig)
 cmd_arc=(defconfig allnoconfig tb10x_defconfig)
 cmd_arcv2=(axs103_defconfig nsim_hs_smp_defconfig vdk_hs38_smp_defconfig)
 cmd_arm=(allmodconfig allnoconfig s3c2410_defconfig omap2plus_defconfig imx_v6_v7_defconfig \
-	ixp4xx_defconfig u8500_defconfig multi_v5_defconfig multi_v7_defconfig omap1_defconfig \
+	ixp4xx_defconfig u8500_defconfig multi_v5_defconfig omap1_defconfig \
 	footbridge_defconfig davinci_all_defconfig mini2440_defconfig \
 	rpc_defconfig axm55xx_defconfig mxs_defconfig keystone_defconfig \
 	vexpress_defconfig imx_v4_v5_defconfig at91_dt_defconfig \
@@ -17,7 +17,7 @@ cmd_arm=(allmodconfig allnoconfig s3c2410_defconfig omap2plus_defconfig imx_v6_v
 	exynos_defconfig cm_x2xx_defconfig s5pv210_defconfig \
 	integrator_defconfig efm32_defconfig \
 	pxa910_defconfig clps711x_defconfig)
-cmd_arm64=(defconfig allnoconfig allmodconfig)
+cmd_arm64=(allnoconfig allmodconfig)
 cmd_avr32=(defconfig allnoconfig merisc_defconfig atngw100mkii_evklcd101_defconfig)
 cmd_blackfin=(defconfig BF561-EZKIT-SMP_defconfig)
 cmd_c6x=(dsk6455_defconfig evmc6457_defconfig evmc6678_defconfig)
@@ -35,7 +35,7 @@ cmd_m68k_nommu=(m5272c3_defconfig m5307c3_defconfig m5249evb_defconfig \
 cmd_metag=(defconfig allnoconfig meta1_defconfig meta2_defconfig meta2_smp_defconfig)
 cmd_microblaze=(mmu_defconfig nommu_defconfig allnoconfig)
 cmd_mips=(defconfig allmodconfig allnoconfig bcm47xx_defconfig bcm63xx_defconfig \
-	nlm_xlp_defconfig ath79_defconfig ar7_defconfig fuloong2e_defconfig \
+	nlm_xlp_defconfig ath79_defconfig ar7_defconfig \
 	e55_defconfig cavium_octeon_defconfig malta_defconfig)
 cmd_mn10300=(asb2303_defconfig asb2364_defconfig)
 cmd_nios2=(3c120_defconfig)
@@ -44,14 +44,14 @@ cmd_parisc=(defconfig allnoconfig allmodconfig generic-32bit_defconfig)
 cmd_parisc64=(a500_defconfig generic-64bit_defconfig)
 cmd_powerpc=(defconfig allmodconfig allnoconfig ppc64e_defconfig cell_defconfig \
 	maple_defconfig ppc6xx_defconfig mpc83xx_defconfig \
-	mpc85xx_defconfig mpc85xx_smp_defconfig tqm8xx_defconfig \
+	tqm8xx_defconfig \
 	85xx/sbc8548_defconfig 83xx/mpc834x_mds_defconfig \
 	86xx/sbc8641d_defconfig)
 cmd_s390=(defconfig allmodconfig allnoconfig)
 cmd_score=(defconfig)
 cmd_sh=(defconfig dreamcast_defconfig microdev_defconfig)
-cmd_sparc32=(defconfig allmodconfig allnoconfig)
-cmd_sparc64=(defconfig allmodconfig allnoconfig)
+cmd_sparc32=(allmodconfig allnoconfig)
+cmd_sparc64=(allmodconfig allnoconfig)
 cmd_tile=(tilegx_defconfig)
 cmd_x86_64=(defconfig allyesconfig allmodconfig allnoconfig)
 cmd_xtensa=(defconfig allmodconfig allnoconfig)
