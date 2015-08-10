7b7b9da277d1f6f913699edc3984279977e888af
Guenter Roeck <linux@roeck-us.net>
2015-08-09 21:00:32 -0700

    rootfs: mips64: Use fixup mechanism to configure SMP mode
    Using the fixup mechanism to configure SMP mode lets us drop
    a configration file.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/mips64/run-qemu-mips64.sh
+++ rootfs/mips64/run-qemu-mips64.sh
@@ -14,17 +14,37 @@ dir=$(cd $(dirname $0); pwd)
 
 . ${dir}/../scripts/common.sh
 
+patch_defconfig()
+{
+    local defconfig=$1
+    local fixup=$2
+
+    # Enable SMP if requested
+
+    sed -i -e '/CONFIG_MIPS_MT_SMP/d' ${defconfig}
+    sed -i -e '/CONFIG_SCHED_SMT/d' ${defconfig}
+    sed -i -e '/CONFIG_NR_CPUS/d' ${defconfig}
+
+    if [ "${fixup}" = "smp" ]
+    then
+        echo "CONFIG_MIPS_MT_SMP=y" >> ${defconfig}
+        echo "CONFIG_SCHED_SMT=y" >> ${defconfig}
+        echo "CONFIG_NR_CPUS=8" >> ${defconfig}
+    fi
+}
+
 runkernel()
 {
     local defconfig=$1
+    local fixup=$2
     local pid
     local retcode
     local logfile=/tmp/runkernel-$$.log
     local waitlist=("Boot successful" "Rebooting")
 
-    echo -n "Building mips64:${defconfig} ... "
+    echo -n "Building mips64:${fixup}:${defconfig} ... "
 
-    dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig}
+    dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig} "" ${fixup}
     if [ $? -ne 0 ]
     then
 	return 1
@@ -48,9 +68,9 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel qemu_mips_malta64_defconfig
+runkernel qemu_mips_malta64_defconfig nosmp
 retcode=$?
-runkernel qemu_mips_malta64_smp_defconfig
+runkernel qemu_mips_malta64_defconfig smp
 retcode=$((${retcode} + $?))
 
 exit ${retcode}
