f194a166579ea7e80fe625f388e372264efecd00
Guenter Roeck <linux@roeck-us.net>
2015-08-09 17:46:23 -0700

    rootfs: ppc64: Add pseries to list of tests
    Main difference is that it needs a different console. Also, the command
    line _must_ include "-vga none", or the system will hang during boot.
    
    pseries_defconfig works for the most part as-is. Only exception is that
    DEVTMPFS has to be enabled explicitly for older kernel versions.
    
    The pseries test also works for 3.2, so add skip entries for 3.2
    to skip the other tests. The 3.2 test will be enabled in buildbot
    with a separate commit.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/ppc64/run-qemu-ppc64.sh
+++ rootfs/ppc64/run-qemu-ppc64.sh
@@ -12,6 +12,7 @@ dir=$(cd $(dirname $0); pwd)
 
 . ${dir}/../scripts/common.sh
 
+skip_32="powerpc:qemu_ppc64_book3s_defconfig powerpc:qemu_ppc64_e5500_defconfig"
 skip_34="powerpc:qemu_ppc64_e5500_defconfig"
 skip_310="powerpc:qemu_ppc64_e5500_defconfig"
 skip_312="powerpc:qemu_ppc64_e5500_defconfig"
@@ -22,20 +23,23 @@ patch_defconfig()
     local defconfig=$1
     local fixup=$2
 
-    # Enable SMP as requested
-
-    sed -i -e '/CONFIG_SMP/d' ${defconfig}
-    sed -i -e '/CONFIG_NR_CPUS/d' ${defconfig}
-
-    if [ "${fixup}" = "nosmp" ]
+    if [ "${fixup}" = "devtmpfs" ]
     then
+        sed -i -e '/CONFIG_DEVTMPFS/d' ${defconfig}
+        echo "CONFIG_DEVTMPFS=y" >> ${defconfig}
+    elif [ "${fixup}" = "nosmp" ]
+    then
+        sed -i -e '/CONFIG_SMP/d' ${defconfig}
         echo "# CONFIG_SMP is not set" >> ${defconfig}
     elif [ "${fixup}" = "smp4" ]
     then
+        sed -i -e '/CONFIG_SMP/d' ${defconfig}
+        sed -i -e '/CONFIG_NR_CPUS/d' ${defconfig}
         echo "CONFIG_SMP=y" >> ${defconfig}
         echo "CONFIG_NR_CPUS=4" >> ${defconfig}
     elif [ "${fixup}" = "smp" ]
     then
+        sed -i -e '/CONFIG_SMP/d' ${defconfig}
         echo "CONFIG_SMP=y" >> ${defconfig}
     fi
 }
@@ -46,16 +50,23 @@ runkernel()
     local fixup=$2
     local machine=$3
     local cpu=$4
-    local kernel=$5
-    local rootfs=$6
-    local reboot=$7
-    local dt=$8
+    local console=$5
+    local kernel=$6
+    local rootfs=$7
+    local reboot=$8
+    local dt=$9
     local pid
     local retcode
     local logfile=/tmp/runkernel-$$.log
     local waitlist=("Restarting system" "Restarting" "Boot successful" "Rebooting")
+    local fixup_msg
 
-    echo -n "Building ${ARCH}:${fixup}:${defconfig} ... "
+    if [ "${fixup}" != "devtmpfs" ]
+    then
+        fixup_msg=":${fixup}"
+    fi
+
+    echo -n "Building ${ARCH}${fixup_msg}:${defconfig} ... "
 
     dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig} "" ${fixup}
     retcode=$?
@@ -78,8 +89,8 @@ runkernel()
 
     /opt/buildbot/bin/qemu-system-ppc64 -M ${machine} -cpu ${cpu} -m 1024 \
     	-kernel ${kernel} -initrd $(basename ${rootfs}) \
-	-nographic -monitor null -no-reboot \
-	--append "rdinit=/sbin/init console=tty console=ttyS0 doreboot" \
+	-nographic -vga none -monitor null -no-reboot \
+	--append "rdinit=/sbin/init console=tty console=${console} doreboot" \
 	${dt_cmd} > ${logfile} 2>&1 &
 
     pid=$!
@@ -93,16 +104,19 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel qemu_ppc64_book3s_defconfig nosmp mac99 ppc64 vmlinux \
+runkernel qemu_ppc64_book3s_defconfig nosmp mac99 ppc64 ttyS0 vmlinux \
 	busybox-powerpc64.img manual
 retcode=$?
-runkernel qemu_ppc64_book3s_defconfig smp4 mac99 ppc64 vmlinux \
+runkernel qemu_ppc64_book3s_defconfig smp4 mac99 ppc64 ttyS0 vmlinux \
 	busybox-powerpc64.img manual
 retcode=$((${retcode} + $?))
-runkernel qemu_ppc64_e5500_defconfig nosmp mpc8544ds e5500 arch/powerpc/boot/uImage \
+runkernel pseries_defconfig devtmpfs pseries POWER8 hvc0 vmlinux \
+	busybox-powerpc64.img auto
+retcode=$((${retcode} + $?))
+runkernel qemu_ppc64_e5500_defconfig nosmp mpc8544ds e5500 ttyS0 arch/powerpc/boot/uImage \
 	../ppc/busybox-ppc.cpio auto "dt_compatible=fsl,,P5020DS"
 retcode=$((${retcode} + $?))
-runkernel qemu_ppc64_e5500_defconfig smp mpc8544ds e5500 arch/powerpc/boot/uImage \
+runkernel qemu_ppc64_e5500_defconfig smp mpc8544ds e5500 ttyS0 arch/powerpc/boot/uImage \
 	../ppc/busybox-ppc.cpio auto "dt_compatible=fsl,,P5020DS"
 retcode=$((${retcode} + $?))
 
