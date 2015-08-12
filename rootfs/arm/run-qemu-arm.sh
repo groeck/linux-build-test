ce57dfc9389009856367d3b27ce70d9649fb440f
Guenter Roeck <linux@roeck-us.net>
2015-08-12 12:33:35 -0700

    rootfs: arm: Add various new builds
    New builds:
    
    versatile_defconfig:versatileab
    versatile_defconfig:versatilepb
    multi_v7_defconfig:smdkc210
    
    Update smdkc210 builds to disable CPU_IDLE to improve stability.
    
    Change order of entries in reboot waitlist. dowait must wait until
    "Restarting" is displayed to avoid errors seen if restart takes
    to long, especially on slow machines.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/arm/run-qemu-arm.sh
+++ rootfs/arm/run-qemu-arm.sh
@@ -9,26 +9,65 @@ PATH=${PATH_ARM}:${PATH}
 
 dir=$(cd $(dirname $0); pwd)
 
-skip_32="arm:vexpress-a15:qemu_arm_vexpress_defconfig \
+# multi_v7_defconfig only exists starting with v3.10.
+# versatileab/versatilepb need different binaries prior to 3.14.
+
+skip_32="arm:smdkc210:exynos_defconfig \
+	arm:smdkc210:multi_v7_defconfig \
+	arm:versatileab:versatile_defconfig \
+	arm:versatilepb:versatile_defconfig \
 	arm:vexpress-a9:multi_v7_defconfig \
 	arm:vexpress-a15:multi_v7_defconfig \
-	arm:smdkc210:exynos_defconfig \
-	arm:xilinx-zynq-a9:multi_v7_defconfig"
-skip_34="arm:versatilepb:qemu_arm_versatile_defconfig \
 	arm:vexpress-a15:qemu_arm_vexpress_defconfig \
+	arm:xilinx-zynq-a9:multi_v7_defconfig"
+skip_34="arm:smdkc210:exynos_defconfig \
+	arm:smdkc210:multi_v7_defconfig \
+	arm:versatileab:versatile_defconfig \
+	arm:versatilepb:versatile_defconfig \
+	arm:versatilepb-qemu:qemu_arm_versatile_defconfig \
 	arm:vexpress-a9:multi_v7_defconfig \
 	arm:vexpress-a15:multi_v7_defconfig \
-	arm:smdkc210:exynos_defconfig \
+	arm:vexpress-a15:qemu_arm_vexpress_defconfig \
 	arm:xilinx-zynq-a9:multi_v7_defconfig"
-skip_310="arm:vexpress-a9:multi_v7_defconfig \
+skip_310="arm:smdkc210:multi_v7_defconfig \
+	arm:versatileab:versatile_defconfig \
+	arm:versatilepb:versatile_defconfig \
+	arm:vexpress-a9:multi_v7_defconfig \
 	arm:vexpress-a15:multi_v7_defconfig \
 	arm:xilinx-zynq-a9:multi_v7_defconfig"
-skip_312="arm:xilinx-zynq-a9:multi_v7_defconfig"
-skip_314="arm:xilinx-zynq-a9:multi_v7_defconfig"
+skip_312="arm:smdkc210:multi_v7_defconfig \
+	arm:versatileab:versatile_defconfig \
+	arm:versatilepb:versatile_defconfig \
+	arm:xilinx-zynq-a9:multi_v7_defconfig"
+skip_314="arm:smdkc210:multi_v7_defconfig \
+	arm:xilinx-zynq-a9:multi_v7_defconfig"
+skip_318="arm:smdkc210:multi_v7_defconfig"
 
 . ${dir}/../scripts/common.sh
 
 cached_config=""
+
+patch_defconfig()
+{
+    local defconfig=$1
+    local fixup=$2
+
+    # We need DEVTMPFS for initrd images.
+
+    if [ "${fixup}" = "devtmpfs" ]
+    then
+	sed -i -e '/CONFIG_DEVTMPFS/d' ${defconfig}
+	echo "CONFIG_DEVTMPFS=y" >> ${defconfig}
+    fi
+
+    # CPUIDLE causes Exynos targets to run really slow.
+
+    if [ "${fixup}" = "cpuidle" ]
+    then
+	sed -i -e '/CONFIG_CPU_IDLE/d' ${defconfig}
+	sed -i -e '/CONFIG_ARM_EXYNOS_CPUIDLE/d' ${defconfig}
+    fi
+}
 
 runkernel()
 {
@@ -37,12 +76,13 @@ runkernel()
     local cpu=$3
     local rootfs=$4
     local mode=$5
-    local dtb=$6
+    local fixup=$6
+    local dtb=$7
     local dtbfile="arch/arm/boot/dts/${dtb}"
     local pid
     local retcode
     local logfile=/tmp/runkernel-$$.log
-    local waitlist=("Boot successful" "Rebooting" "Restarting")
+    local waitlist=("Restarting" "Boot successful" "Rebooting")
     local rel=$(git describe | cut -f1 -d- | cut -f1,2 -d. | sed -e 's/\.//' | sed -e 's/v//')
     local tmp="skip_${rel}"
     local skip=(${!tmp})
@@ -52,7 +92,7 @@ runkernel()
 
     if [ -n "${dtb}" ]
     then
-        pbuild="${build}:$(echo ${dtb} | sed -e 's/.dtb//')"
+	pbuild="${build}:$(echo ${dtb} | sed -e 's/.dtb//')"
     fi
 
     echo -n "Building ${pbuild} ... "
@@ -66,22 +106,22 @@ runkernel()
 	fi
     done
 
-    if [ "${cached_config}" != "${defconfig}" ]
+    if [ "${cached_config}" != "${defconfig}:${fixup}" ]
     then
 	# KALLSYMS_EXTRA_PASS is needed for earlier kernels (3.2, 3.4) due to
 	# a bug in kallsyms which would be too difficult to back-port.
 	# See upstream commits f6537f2f0e and 7122c3e915.
-	dosetup ${ARCH} ${PREFIX} "KALLSYMS_EXTRA_PASS=1" ${rootfs} ${defconfig}
+	dosetup ${ARCH} ${PREFIX} "KALLSYMS_EXTRA_PASS=1" ${rootfs} ${defconfig} "" ${fixup}
 	retcode=$?
 	if [ ${retcode} -ne 0 ]
 	then
 	    return 1
 	fi
     else
-        setup_rootfs ${rootfs}
+	setup_rootfs ${rootfs}
     fi
 
-    cached_config=${defconfig}
+    cached_config="${defconfig}:${fixup}"
 
     echo -n "running ..."
 
@@ -118,8 +158,15 @@ runkernel()
 	    ${dtbcmd} > ${logfile} 2>&1 &
 	pid=$!
 	;;
-    "realview-pb-a8" | "realview-eb-mpcore" | "realview-eb")
-	/opt/buildbot/bin/qemu-system-arm -M ${mach} ${cpucmd} -m 512 \
+    "realview-pb-a8" | "realview-eb-mpcore" | "realview-eb" | \
+    "versatileab" | "versatilepb" )
+	local mem=512
+	if [ "${mach}" = "versatileab" -o "${mach}" = "versatilepb" ]
+	then
+	    # 512 is too much for versatile, may not be enough for realview
+	    mem=128
+	fi
+	/opt/buildbot/bin/qemu-system-arm -M ${mach} ${cpucmd} -m ${mem} \
 	    -kernel arch/arm/boot/zImage -no-reboot \
 	    -initrd ${rootfs} \
 	    --append "rdinit=/sbin/init console=ttyAMA0,115200 doreboot" \
@@ -127,8 +174,8 @@ runkernel()
 	    ${dtbcmd} > ${logfile} 2>&1 &
 	pid=$!
 	;;
-    "versatilepb")
-	/opt/buildbot/bin/qemu-system-arm -M ${mach} -m 128 \
+    "versatilepb-qemu")
+	/opt/buildbot/bin/qemu-system-arm -M versatilepb -m 128 \
 	    -kernel arch/arm/boot/zImage -no-reboot \
 	    -drive file=${rootfs},if=scsi \
 	    --append "root=/dev/sda rw mem=128M console=ttyAMA0,115200 console=tty doreboot" \
@@ -158,39 +205,52 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel qemu_arm_versatile_defconfig versatilepb "" \
+runkernel qemu_arm_versatile_defconfig versatilepb-qemu "" \
 	core-image-minimal-qemuarm.ext3 auto
 retcode=$?
+
+runkernel versatile_defconfig versatileab "" \
+	core-image-minimal-qemuarm.cpio auto devtmpfs
+retcode=$((${retcode} + $?))
+runkernel versatile_defconfig versatilepb "" \
+	core-image-minimal-qemuarm.cpio auto devtmpfs
+retcode=$((${retcode} + $?))
+
 runkernel qemu_arm_vexpress_defconfig vexpress-a9 "" \
-	core-image-minimal-qemuarm.ext3 auto vexpress-v2p-ca9.dtb
+	core-image-minimal-qemuarm.ext3 auto "" vexpress-v2p-ca9.dtb
 retcode=$((${retcode} + $?))
 runkernel qemu_arm_vexpress_defconfig vexpress-a15 "" \
-	core-image-minimal-qemuarm.ext3 auto vexpress-v2p-ca15-tc1.dtb
+	core-image-minimal-qemuarm.ext3 auto "" vexpress-v2p-ca15-tc1.dtb
 retcode=$((${retcode} + $?))
+
 runkernel multi_v7_defconfig vexpress-a9 "" \
-	core-image-minimal-qemuarm.ext3 auto vexpress-v2p-ca9.dtb
+	core-image-minimal-qemuarm.ext3 auto "" vexpress-v2p-ca9.dtb
 retcode=$((${retcode} + $?))
 runkernel multi_v7_defconfig vexpress-a15 "" \
-	core-image-minimal-qemuarm.ext3 auto vexpress-v2p-ca15-tc1.dtb
+	core-image-minimal-qemuarm.ext3 auto "" vexpress-v2p-ca15-tc1.dtb
 retcode=$((${retcode} + $?))
 
 runkernel multi_v7_defconfig xilinx-zynq-a9 "" \
-	core-image-minimal-qemuarm.ext3 auto zynq-zc702.dtb
+	core-image-minimal-qemuarm.ext3 auto "" zynq-zc702.dtb
 retcode=$((${retcode} + $?))
 runkernel multi_v7_defconfig xilinx-zynq-a9 "" \
-	core-image-minimal-qemuarm.ext3 auto zynq-zc706.dtb
+	core-image-minimal-qemuarm.ext3 auto "" zynq-zc706.dtb
 retcode=$((${retcode} + $?))
 runkernel multi_v7_defconfig xilinx-zynq-a9 "" \
-	core-image-minimal-qemuarm.ext3 auto zynq-zed.dtb
+	core-image-minimal-qemuarm.ext3 auto "" zynq-zed.dtb
 retcode=$((${retcode} + $?))
 
+runkernel multi_v7_defconfig smdkc210 "" \
+	core-image-minimal-qemuarm.cpio manual cpuidle exynos4210-smdkv310.dtb
+retcode=$((${retcode} + $?))
 runkernel exynos_defconfig smdkc210 "" \
-	core-image-minimal-qemuarm.cpio manual exynos4210-smdkv310.dtb
+	core-image-minimal-qemuarm.cpio manual cpuidle exynos4210-smdkv310.dtb
 retcode=$((${retcode} + $?))
 
 runkernel qemu_arm_realview_pb_defconfig realview-pb-a8 "" \
 	busybox-arm.cpio auto
 retcode=$((${retcode} + $?))
+
 runkernel qemu_arm_realview_eb_defconfig realview-eb-mpcore "" \
 	core-image-minimal-qemuarm.cpio manual
 retcode=$((${retcode} + $?))
