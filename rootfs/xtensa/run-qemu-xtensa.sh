d751d1c8af4c2a28687f43b2947a94440a579fb8
Guenter Roeck <linux@roeck-us.net>
2015-08-13 12:20:23 -0700

    rootfs: xtensa: Test all supported boards
    qemu supports several different evaluation boards.
    Run the test with all of them.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/xtensa/run-qemu-xtensa.sh
+++ rootfs/xtensa/run-qemu-xtensa.sh
@@ -11,25 +11,32 @@ dir=$(cd $(dirname $0); pwd)
 
 . ${dir}/../scripts/common.sh
 
+cached_defconfig=""
+
 runkernel()
 {
     local defconfig=$1
+    local mach=$2
     local pid
     local retcode
     local logfile=/tmp/runkernel-$$.log
     local waitlist=("Restarting system" "Boot successful" "Rebooting")
 
-    echo -n "Building ${ARCH}:${defconfig} ... "
+    echo -n "Building ${ARCH}:${mach}:${defconfig} ... "
 
-    dosetup ${ARCH} ${PREFIX} "bootdir-y=boot-elf" ${rootfs} ${defconfig}
-    if [ $? -ne 0 ]
+    if [ "${cached_defconfig}" != "${defconfig}" ]
     then
-	return 1
+        dosetup ${ARCH} ${PREFIX} "bootdir-y=boot-elf" ${rootfs} ${defconfig}
+        if [ $? -ne 0 ]
+        then
+	    return 1
+        fi
+	cached_defconfig=${defconfig}
     fi
 
     echo -n "running ..."
 
-    /opt/buildbot/bin/qemu-system-xtensa -cpu dc232b -M lx60 \
+    /opt/buildbot/bin/qemu-system-xtensa -cpu dc232b -M ${mach} \
 	-kernel arch/xtensa/boot/Image.elf -no-reboot \
 	 -append 'rdinit=/sbin/init console=ttyS0 console=tty' \
 	-m 128M -nographic -monitor null -serial stdio \
@@ -45,7 +52,13 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel qemu_xtensa_defconfig
+runkernel qemu_xtensa_defconfig lx60
 retcode=$?
+runkernel qemu_xtensa_defconfig lx200
+retcode=$((${retcode} + $?))
+runkernel qemu_xtensa_defconfig kc705
+retcode=$((${retcode} + $?))
+runkernel qemu_xtensa_defconfig ml605
+retcode=$((${retcode} + $?))
 
 exit ${retcode}
