240b007d4f156a58627b81ed1b9bdb97b8aabc4a
Guenter Roeck <linux@roeck-us.net>
2015-08-18 07:08:58 -0700

    rootfs: xtensa: Drop lx200 and ml605, add dc233c
    lx200 and ml605 tests don't really add value per xtensa maintainer.
    Add kc605 test with dc233c CPU instead.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/xtensa/run-qemu-xtensa.sh
+++ rootfs/xtensa/run-qemu-xtensa.sh
@@ -3,7 +3,7 @@
 PREFIX=xtensa-linux-
 ARCH=xtensa
 rootfs=busybox-xtensa.cpio
-PATH_XTENSA=/opt/kernel/xtensa/gcc-4.8.3-dc232b/usr/bin
+PATH_XTENSA=/opt/kernel/xtensa/gcc-4.9.2-dc233c/usr/bin
 
 PATH=${PATH_XTENSA}:${PATH}
 
@@ -13,33 +13,71 @@ dir=$(cd $(dirname $0); pwd)
 
 cached_defconfig=""
 
+skip_314="xtensa:generic_kc705_defconfig"
+
+patch_defconfig()
+{
+    local defconfig=$1
+    local fixup=$2
+    local progdir=$(cd $(dirname $0); pwd)
+
+    # Always provide initrd
+
+    sed -i -e '/CONFIG_INITRAMFS_SOURCE/d' ${defconfig}
+    echo 'CONFIG_INITRAMFS_SOURCE="busybox-xtensa.cpio"' >> ${defconfig}
+
+    # Specify built-in devicetree file as required for configuration
+    # Copy devicetree file here since 'dosetup' will otherwise remove it
+    # during its clean-up phase.
+
+    if [ "${fixup}" != "initrd" -a -e "${progdir}/${fixup}" ]
+    then
+        cp ${progdir}/${fixup} arch/${ARCH}/boot/dts/qemu.dts
+        sed -i -e '/CONFIG_BUILTIN_DTB/d' ${defconfig}
+	echo 'CONFIG_BUILTIN_DTB="qemu"' >> ${defconfig}
+    fi
+}
+
 runkernel()
 {
     local defconfig=$1
-    local mach=$2
+    local dts=$2
+    local cpu=$3
+    local mach=$4
+    local mem=$5
     local pid
     local retcode
     local logfile=/tmp/runkernel-$$.log
     local waitlist=("Restarting system" "Boot successful" "Rebooting")
+    local fixup="initrd"
 
-    echo -n "Building ${ARCH}:${mach}:${defconfig} ... "
+    echo -n "Building ${ARCH}:${cpu}:${mach}:${defconfig} ... "
 
-    if [ "${cached_defconfig}" != "${defconfig}" ]
+    if [ -n "${dts}" ]
     then
-        dosetup ${ARCH} ${PREFIX} "bootdir-y=boot-elf" ${rootfs} ${defconfig}
-        if [ $? -ne 0 ]
+	fixup="${dts}"
+    fi
+
+    if [ "${cached_defconfig}" != "${defconfig}:${cpu}:${dts}" ]
+    then
+        dosetup ${ARCH} ${PREFIX} "bootdir-y=boot-elf" ${rootfs} ${defconfig} "" ${fixup}
+	retcode=$?
+        if [ ${retcode} -ne 0 ]
         then
+	    if [ ${retcode} -eq 2 ]
+	    then
+	        return 0
+	    fi
 	    return 1
         fi
-	cached_defconfig=${defconfig}
+	cached_defconfig="${defconfig}:${cpu}:${dts}"
     fi
 
     echo -n "running ..."
 
-    /opt/buildbot/bin/qemu-system-xtensa -cpu dc232b -M ${mach} \
+    /opt/buildbot/bin/qemu-system-xtensa -cpu ${cpu} -M ${mach} \
 	-kernel arch/xtensa/boot/Image.elf -no-reboot \
-	 -append 'rdinit=/sbin/init console=ttyS0 console=tty' \
-	-m 128M -nographic -monitor null -serial stdio \
+	-m ${mem} -nographic -monitor null -serial stdio \
 	> ${logfile} 2>&1 &
 
     pid=$!
@@ -52,13 +90,11 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel qemu_xtensa_defconfig lx60
+runkernel qemu_xtensa_defconfig "" dc232b lx60 128M
 retcode=$?
-runkernel qemu_xtensa_defconfig lx200
+runkernel qemu_xtensa_defconfig "" dc232b kc705 1G
 retcode=$((${retcode} + $?))
-runkernel qemu_xtensa_defconfig kc705
-retcode=$((${retcode} + $?))
-runkernel qemu_xtensa_defconfig ml605
+runkernel generic_kc705_defconfig qemu_kc705.dts dc233c kc705 1G
 retcode=$((${retcode} + $?))
 
 exit ${retcode}
