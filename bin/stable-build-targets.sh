b40ba643fde6725c232ef26ea8d1215337280fee
Guenter Roeck <linux@roeck-us.net>
2015-09-13 18:34:00 -0700

    stable-build-targets.sh: Drop rpc_defconfig
    Doesn't build the latest kernel (4.3-rc1+) due to an internal compiler
    error, and it appears that there are no plans to implement a workaround.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/stable-build-targets.sh
+++ bin/stable-build-targets.sh
@@ -10,7 +10,7 @@ cmd_arcv2=(axs103_defconfig nsim_hs_smp_defconfig vdk_hs38_smp_defconfig)
 cmd_arm=(allmodconfig allnoconfig s3c2410_defconfig omap2plus_defconfig imx_v6_v7_defconfig \
 	ixp4xx_defconfig u8500_defconfig multi_v5_defconfig omap1_defconfig \
 	footbridge_defconfig davinci_all_defconfig mini2440_defconfig \
-	rpc_defconfig axm55xx_defconfig mxs_defconfig keystone_defconfig \
+	axm55xx_defconfig mxs_defconfig keystone_defconfig \
 	vexpress_defconfig imx_v4_v5_defconfig at91_dt_defconfig \
 	s3c6400_defconfig lpc32xx_defconfig shmobile_defconfig \
 	nhk8815_defconfig bcm2835_defconfig sama5_defconfig orion5x_defconfig \
