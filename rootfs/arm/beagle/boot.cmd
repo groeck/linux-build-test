0f075595e589cff530d45e5308edd2125b6b1dd3
Guenter Roeck <linux@roeck-us.net>
2015-11-11 12:56:49 -0800

    rootfs: arm: Move dtb load address higher for beagle boards
    multi_v7_defconfig is getting too large and needs more space.
    Move dtb load address for beagle higher to avoid boot failures.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/arm/beagle/boot.cmd
+++ rootfs/arm/beagle/boot.cmd
@@ -1,6 +1,6 @@
 setenv fdt_high "0xffffffff"
 setenv bootargs "console=ttyO2,115200n8 root=${mmcroot} rootwait earlyprintk fixrtc nocompcache vram=12M omapfb.mode=${dvimode} mpurate=${mpurate} doreboot"
 fatload mmc 0:1 0x80000000 uImage
-fatload mmc 0:1 0x815f0000 devicetree.dtb
+fatload mmc 0:1 0x84000000 devicetree.dtb
 echo "Booting from boot.scr"
-bootm 0x80000000 - 0x815f0000
+bootm 0x80000000 - 0x84000000
