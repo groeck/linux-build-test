327e365500bf84188411adbf55d50d200622d764
Guenter Roeck <linux@roeck-us.net>
2015-12-07 06:43:52 -0800

    Improve cleanup script
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,4 +1,4 @@
-# Remove all log files older than 30 days
+# Remove all log files older than 40 days
 
 cd /opt/buildbot/master
-rm $(find mmotm-* qemu-* stable-queue-* hwmon-* master-* next-* -type f -ctime +30 | grep -v workdir)
+rm $(find mmotm-* qemu-* stable-updates stable-queue-* hwmon* master-* next-* -type f -ctime +40 | grep -v workdir)
