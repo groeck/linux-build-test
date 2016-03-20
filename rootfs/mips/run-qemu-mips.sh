cf5e60ddbd3bcd6d9b3dc068c2d7f244f4f100c3
Guenter Roeck <linux@roeck-us.net>
2016-03-20 08:22:11 -0700

    rootfs: mips: Use standard configuration
    Replace qemu specific configuration with malta_defconfig.
    Also replace yocto root file system with minimal busybox
    based root file system.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/mips/run-qemu-mips.sh
+++ rootfs/mips/run-qemu-mips.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 config=$1
+variant=$2
 
 # machine specific information
-rootfs=core-image-minimal-qemumips.ext3
+rootfs=busybox-mips.ext3
 PATH_MIPS=/opt/poky/1.3/sysroots/x86_64-pokysdk-linux/usr/bin/mips32-poky-linux
 PATH_X86=/opt/poky/1.4.0-1/sysroots/x86_64-pokysdk-linux/usr/bin
 PREFIX=mips-poky-linux-
@@ -17,14 +18,40 @@ dir=$(cd $(dirname $0); pwd)
 
 . ${dir}/../scripts/common.sh
 
+patch_defconfig()
+{
+    local defconfig=$1
+    local fixup=$2
+
+    # Enable DEVTMPFS
+    sed -i -e '/CONFIG_DEVTMPFS/d' ${defconfig}
+    echo "CONFIG_DEVTMPFS=y" >> ${defconfig}
+    # Enable BLK_DEV_INITRD for initrd support
+    # sed -i -e '/CONFIG_BLK_DEV_INITRD/d' ${defconfig}
+    # echo "CONFIG_BLK_DEV_INITRD=y" >> ${defconfig}
+
+    # Build a big endian image
+    sed -i -e '/CONFIG_CPU_LITTLE_ENDIAN/d' ${defconfig}
+    sed -i -e '/CONFIG_CPU_BIG_ENDIAN/d' ${defconfig}
+    echo "CONFIG_CPU_BIG_ENDIAN=y" >> ${defconfig}
+
+    sed -i -e '/CONFIG_MIPS_MT_SMP/d' ${defconfig}
+    if [ "${fixup}" = "smp" ]
+    then
+        echo "CONFIG_MIPS_MT_SMP=y" >> ${defconfig}
+    fi
+}
+
 runkernel()
 {
     local defconfig=$1
+    local fixup=$2
     local pid
     local retcode
     local logfile=/tmp/runkernel-$$.log
+    local mountdir="/dev/sda"
     local waitlist=("Boot successful" "Rebooting")
-    local build="${ARCH}:${defconfig}"
+    local build="${ARCH}:${defconfig}:${fixup}"
 
     if [ -n "${config}" -a "${config}" != "${defconfig}" ]
     then
@@ -32,20 +59,36 @@ runkernel()
 	return 0
     fi
 
+    if [ -n "${variant}" -a "${variant}" != "${fixup}" ]
+    then
+	echo "Skipping ${build} ... "
+	return 0
+    fi
+
     echo -n "Building ${build} ... "
 
-    dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig}
+    dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig} "" ${fixup}
     if [ $? -ne 0 ]
     then
 	return 1
     fi
 
+    # The actual configuration determines if the root file system
+    # is /dev/sda (CONFIG_ATA) or /dev/hda (CONFIG_IDE).
+    # CONFIG_ATA is enabled in kernel version 4.1 and later.
+    grep "CONFIG_ATA=y" .config >/dev/null 2>&1
+    if [ $? -ne 0 ]
+    then
+	mountdir="/dev/hda"
+    fi
+
     echo -n "running ..."
 
-    ${QEMUCMD} -kernel ${KERNEL_IMAGE} -M ${QEMU_MACH} -hda ${rootfs} \
+    ${QEMUCMD} -kernel ${KERNEL_IMAGE} -M ${QEMU_MACH} \
+	-drive file=${rootfs},format=raw,if=ide \
 	-vga cirrus -usb -usbdevice wacom-tablet -no-reboot -m 128 \
-	--append "root=/dev/hda rw mem=128M console=ttyS0 console=tty doreboot" \
-	-nographic > ${logfile} 2>&1 &
+	--append "root=${mountdir} rw mem=128M console=ttyS0 console=tty doreboot" \
+	-nographic -monitor none > ${logfile} 2>&1 &
 
     pid=$!
     dowait ${pid} ${logfile} automatic waitlist[@]
@@ -57,9 +100,9 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel qemu_mips_malta_defconfig
+runkernel malta_defconfig nosmp
 retcode=$?
-runkernel qemu_mips_malta_smp_defconfig
+runkernel malta_defconfig smp
 retcode=$((${retcode} + $?))
 
 exit ${retcode}
