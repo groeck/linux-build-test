571e1d2e9e63b7a0caf47c961ca5ee95a8b6d829
Guenter Roeck <linux@roeck-us.net>
2016-03-20 12:06:18 -0700

    rootfs: mips64: Convert to use default configuration
    Use malta_defconfig as base for the test configuration.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/mips64/run-qemu-mips64.sh
+++ rootfs/mips64/run-qemu-mips64.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+config=$1
+variant=$2
+
 # machine specific information
 rootfs=core-image-minimal-qemumips64.ext3
 PATH_MIPS=/opt/kernel/gcc-4.6.3-nolibc/mips64-linux/bin
@@ -18,6 +21,25 @@ patch_defconfig()
 {
     local defconfig=$1
     local fixup=$2
+
+    # Enable DEVTMPFS
+
+    sed -i -e '/CONFIG_DEVTMPFS/d' ${defconfig}
+    echo "CONFIG_DEVTMPFS=y" >> ${defconfig}
+
+    # 64 bit build
+
+    sed -i -e '/CONFIG_CPU_MIPS/d' ${defconfig}
+    sed -i -e '/CONFIG_32BIT/d' ${defconfig}
+    sed -i -e '/CONFIG_64BIT/d' ${defconfig}
+    echo "CONFIG_CPU_MIPS64_R1=y" >> ${defconfig}
+    echo "CONFIG_64BIT=y" >> ${defconfig}
+
+    # Build a big endian image
+
+    sed -i -e '/CONFIG_CPU_LITTLE_ENDIAN/d' ${defconfig}
+    sed -i -e '/CONFIG_CPU_BIG_ENDIAN/d' ${defconfig}
+    echo "CONFIG_CPU_BIG_ENDIAN=y" >> ${defconfig}
 
     # Enable SMP if requested
 
@@ -39,10 +61,24 @@ runkernel()
     local fixup=$2
     local pid
     local retcode
+    local mountdir="/dev/sda"
     local logfile=/tmp/runkernel-$$.log
     local waitlist=("Boot successful" "Rebooting")
+    local build="mips64:${defconfig}:${fixup}"
 
-    echo -n "Building mips64:${fixup}:${defconfig} ... "
+    if [ -n "${config}" -a "${config}" != "${defconfig}" ]
+    then
+	echo "Skipping ${build} ... "
+	return 0
+    fi
+
+    if [ -n "${variant}" -a "${variant}" != "${fixup}" ]
+    then
+	echo "Skipping ${build} ... "
+	return 0
+    fi
+
+    echo -n "Building ${build} ... "
 
     dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig} "" ${fixup}
     if [ $? -ne 0 ]
@@ -50,11 +86,21 @@ runkernel()
 	return 1
     fi
 
+    # The actual configuration determines if the root file system
+    # is /dev/sda (CONFIG_ATA) or /dev/hda (CONFIG_IDE).
+    # CONFIG_ATA is enabled in kernel version 4.1 and later.
+    grep "CONFIG_ATA=y" .config >/dev/null 2>&1
+    if [ $? -ne 0 ]
+    then
+	mountdir="/dev/hda"
+    fi
+
     echo -n "running ..."
 
-    ${QEMUCMD} -kernel ${KERNEL_IMAGE} -M ${QEMU_MACH} -hda ${rootfs} \
+    ${QEMUCMD} -kernel ${KERNEL_IMAGE} -M ${QEMU_MACH} \
+	-drive file=${rootfs},format=raw,if=ide \
 	-vga cirrus -usb -usbdevice wacom-tablet -no-reboot -m 128 \
-	--append "root=/dev/hda rw mem=128M console=ttyS0 console=tty doreboot" \
+	--append "root=${mountdir} rw mem=128M console=ttyS0 console=tty doreboot" \
 	-nographic > ${logfile} 2>&1 &
 
     pid=$!
@@ -68,9 +114,9 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel qemu_mips_malta64_defconfig nosmp
+runkernel malta_defconfig nosmp
 retcode=$?
-runkernel qemu_mips_malta64_defconfig smp
+runkernel malta_defconfig smp
 retcode=$((${retcode} + $?))
 
 exit ${retcode}
