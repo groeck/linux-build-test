d782a38b0ee6aabe8bfca1bc301c868f2de7390d
Guenter Roeck <linux@roeck-us.net>
2016-03-20 07:18:27 -0700

    rootfs: mipsel: Convert to use standard configuration
    Use malta_defconfig as base for builds instead of a home-grown configuration.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/mipsel/run-qemu-mipsel.sh
+++ rootfs/mipsel/run-qemu-mipsel.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+config=$1
+variant=$2
+
 # machine specific information
 rootfs=busybox-mipsel.cpio
 PATH_MIPS=/opt/poky/1.3/sysroots/x86_64-pokysdk-linux/usr/bin/mips32-poky-linux
@@ -15,17 +18,50 @@ dir=$(cd $(dirname $0); pwd)
 
 . ${dir}/../scripts/common.sh
 
+patch_defconfig()
+{
+    local defconfig=$1
+    local fixup=$2
+
+    # Enable DEVTMPFS and BLK_DEV_INITRD for initrd support
+    # DEVTMPFS needs to be explicitly enabled for v3.14 and older kernels.
+    sed -i -e '/CONFIG_DEVTMPFS/d' ${defconfig}
+    echo "CONFIG_DEVTMPFS=y" >> ${defconfig}
+    sed -i -e '/CONFIG_BLK_DEV_INITRD/d' ${defconfig}
+    echo "CONFIG_BLK_DEV_INITRD=y" >> ${defconfig}
+
+    sed -i -e '/CONFIG_MIPS_MT_SMP/d' ${defconfig}
+    if [ "${fixup}" = "smp" ]
+    then
+        echo "CONFIG_MIPS_MT_SMP=y" >> ${defconfig}
+    fi
+}
+
 runkernel()
 {
     local defconfig=$1
+    local fixup=$2
     local pid
     local retcode
     local logfile=/tmp/runkernel-$$.log
     local waitlist=("Boot successful" "Rebooting")
+    local build="${ARCH}:${defconfig}:${fixup}"
 
-    echo -n "Building ${ARCH}:${defconfig} ... "
+    if [ -n "${config}" -a "${config}" != "${defconfig}" ]
+    then
+	echo "Skipping ${build} ... "
+	return 0
+    fi
 
-    dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig}
+    if [ -n "${variant}" -a "${variant}" != "${fixup}" ]
+    then
+	echo "Skipping ${build} ... "
+	return 0
+    fi
+
+    echo -n "Building ${build} ... "
+
+    dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig} "" ${fixup}
     if [ $? -ne 0 ]
     then
 	return 1
@@ -49,9 +85,9 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel qemu_mipsel_malta_defconfig
+runkernel malta_defconfig nosmp
 retcode=$?
-runkernel qemu_mipsel_malta_smp_defconfig
+runkernel malta_defconfig smp
 retcode=$((${retcode} + $?))
 
 exit ${retcode}
