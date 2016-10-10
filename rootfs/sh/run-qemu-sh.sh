b554300f10883d83268a71c0137ef92f2bce36f2
Guenter Roeck <linux@roeck-us.net>
2016-10-10 14:23:24 -0700

    rootfs: sh: Use gcc 4.6.3 for v3.2 and v3.4
    gcc 5.3 doesn't work for those kernels.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/sh/run-qemu-sh.sh
+++ rootfs/sh/run-qemu-sh.sh
@@ -3,8 +3,18 @@
 PREFIX=sh4-linux-
 ARCH=sh
 rootfs=rootfs.ext2
-# PATH_SH=/opt/kernel/gcc-4.6.3-nolibc/sh4-linux/bin
-PATH_SH=/opt/kernel/sh4/gcc-5.3.0/usr/bin
+
+rel=$(git describe | cut -f1 -d- | cut -f1,2 -d.)
+case "${rel}" in
+v3.2|v3.4)
+	PATH_SH=/opt/kernel/gcc-4.6.3-nolibc/sh4-linux/bin
+	;;
+*)
+	PATH_SH=/opt/kernel/sh4/gcc-5.3.0/usr/bin
+	;;
+esac
+
+QEMU=/opt/buildbot/qemu-install/v2.7/bin/qemu-system-sh4
 
 PATH=${PATH_SH}:${PATH}
 
@@ -38,7 +48,7 @@ runkernel()
 
     echo -n "running ..."
 
-    /opt/buildbot/bin/qemu-system-sh4 -M r2d -kernel ./arch/sh/boot/zImage \
+    ${QEMU} -M r2d -kernel ./arch/sh/boot/zImage \
 	-drive file=${rootfs},format=raw,if=ide \
 	-append "root=/dev/sda console=ttySC1,115200 noiotrap doreboot" \
 	-serial null -serial stdio -net nic,model=rtl8139 -net user \
