1a7fa2b691b14471a7a3c9adcc1cb50bf0483a98
Guenter Roeck <linux@roeck-us.net>
2016-10-10 20:48:12 -0700

    rootfs: arm64: Add support for xlnx-ep108
    xlnx-ep108 is supported in qemu 2.7 and from kernel version 4.1+.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/arm64/run-qemu-arm64.sh
+++ rootfs/arm64/run-qemu-arm64.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-build=$1
+machine=$1
 config=$2
 
+QEMU=/opt/buildbot/qemu-install/v2.7/bin/qemu-system-aarch64
 PREFIX=aarch64-linux-
 ARCH=arm64
-rootfs=rootfs.arm64.cpio
 PATH_ARM64=/opt/kernel/aarch64/gcc-5.2/usr/bin
 
 PATH=${PATH_ARM64}:${PATH}
@@ -14,14 +14,27 @@ dir=$(cd $(dirname $0); pwd)
 
 . ${dir}/../scripts/common.sh
 
+skip_32="arm64:xlnx-ep108:smp:defconfig \
+	arm64:xlnx-ep108:nosmp:defconfig"
+skip_34="arm64:xlnx-ep108:smp:defconfig \
+	arm64:xlnx-ep108:nosmp:defconfig"
+skip_310="arm64:xlnx-ep108:smp:defconfig \
+	arm64:xlnx-ep108:nosmp:defconfig"
+skip_312="arm64:xlnx-ep108:smp:defconfig \
+	arm64:xlnx-ep108:nosmp:defconfig"
+skip_316="arm64:xlnx-ep108:smp:defconfig \
+	arm64:xlnx-ep108:nosmp:defconfig"
+skip_318="arm64:xlnx-ep108:smp:defconfig \
+	arm64:xlnx-ep108:nosmp:defconfig"
+
 patch_defconfig()
 {
     local defconfig=$1
-    local smp=$2
+    local fixup=$2
 
     sed -i -e '/CONFIG_SMP/d' ${defconfig}
 
-    if [ "${smp}" = "nosmp" ]
+    if [ "${fixup}" = "nosmp" ]
     then
 	echo "# CONFIG_SMP is not set" >> ${defconfig}
     else
@@ -31,15 +44,29 @@ patch_defconfig()
 
 runkernel()
 {
-    local defconfig=$1
-    local smp=$2
+    local mach=$1
+    local defconfig=$2
+    local fixup=$3
+    local rootfs=$4
+    local dtb=$5
+    local ddtb=$(basename -s .dtb ""${dtb})
+    local dtbfile="arch/arm64/boot/dts/${dtb}"
     local pid
     local retcode
     local logfile=/tmp/runkernel-$$.log
     local waitlist=("Restarting system" "Boot successful" "Rebooting")
-    local pbuild="${ARCH}:${smp}:${defconfig}"
+    local build=${ARCH}:${mach}:${fixup}:${defconfig}
+    local pbuild=${build}
+    local rel=$(git describe | cut -f1 -d- | cut -f1,2 -d. | sed -e 's/\.//' | sed -e 's/v//')
+    local tmp="skip_${rel}"
+    local skip=(${!tmp})
 
-    if [ -n "${build}" -a "${build}" != "${smp}" ]
+    if [ -n "${ddtb}" ]
+    then
+	pbuild="${build}:${ddtb}"
+    fi
+
+    if [ -n "${machine}" -a "${machine}" != "${mach}" ]
     then
 	echo "Skipping ${pbuild} ... "
 	return 0
@@ -53,22 +80,67 @@ runkernel()
 
     echo -n "Building ${pbuild} ... "
 
-    dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig} generic ${smp}
-    if [ $? -ne 0 ]
+    for s in ${skip[*]}
+    do
+	if [ "$s" = "${build}" ]
+	then
+	    echo "skipped"
+	    return 0
+	fi
+    done
+
+    if [ "${cached_config}" != "${defconfig}:${fixup}" ]
     then
-	return 1
+	dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig} generic ${fixup}
+	retcode=$?
+	if [ ${retcode} -eq 2 ]
+	then
+	    return 0
+	fi
+	if [ ${retcode} -ne 0 ]
+	then
+	    return 1
+	fi
+    else
+	setup_rootfs ${rootfs}
+    fi
+
+    cached_config="${defconfig}:${fixup}"
+
+    # if we have a dtb file use it
+    local dtbcmd=""
+    if [ -n "${dtb}" -a -f "${dtbfile}" ]
+    then
+	dtbcmd="-dtb ${dtbfile}"
     fi
 
     echo -n "running ..."
 
-    /opt/buildbot/bin/qemu-system-aarch64 -machine virt -cpu cortex-a57 \
+    case ${mach} in
+    "virt")
+	${QEMU} -machine ${mach} -cpu cortex-a57 \
 	-machine type=virt -nographic -smp 1 -m 512 \
 	-kernel arch/arm64/boot/Image -initrd ${rootfs} -no-reboot \
 	-append "console=ttyAMA0" > ${logfile} 2>&1 &
 
-    pid=$!
-    dowait ${pid} ${logfile} manual waitlist[@]
-    retcode=$?
+	pid=$!
+	dowait ${pid} ${logfile} manual waitlist[@]
+	retcode=$?
+	;;
+    "xlnx-ep108")
+	${QEMU} -M ${mach} -kernel arch/arm64/boot/Image -m 2048 \
+		-nographic -serial mon:stdio \
+		-monitor none \
+		${dtbcmd} \
+		-no-reboot -initrd ${rootfs} \
+		--append "rdinit=/sbin/init console=ttyPS0 doreboot" \
+		> ${logfile} 2>&1 &
+	pid=$!
+	dowait ${pid} ${logfile} automatic waitlist[@]
+	retcode=$?
+	;;
+    esac
+
     rm -f ${logfile}
     return ${retcode}
 }
@@ -76,9 +148,14 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel defconfig smp
+runkernel virt defconfig smp rootfs.arm64.cpio
 retcode=$?
-runkernel defconfig nosmp
+# Needs changes in root file system to correctly handle reboot
+runkernel xlnx-ep108 defconfig smp busybox-arm64.cpio xilinx/zynqmp-ep108.dtb
+retcode=$((${retcode} + $?))
+runkernel virt defconfig nosmp rootfs.arm64.cpio
+retcode=$((${retcode} + $?))
+runkernel xlnx-ep108 defconfig nosmp busybox-arm64.cpio xilinx/zynqmp-ep108.dtb
 retcode=$((${retcode} + $?))
 
 exit ${retcode}
