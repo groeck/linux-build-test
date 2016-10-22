287b20078407adcaa8109f085839f155a5035a03
Guenter Roeck <linux@roeck-us.net>
2016-10-22 14:46:03 -0700

    distribute: Also distribute to virtual configuration
    Prototype: Doesn't work well. Needs update.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/distribute.sh
+++ bin/distribute.sh
@@ -8,6 +8,7 @@ then
 fi
 
 base=/opt/buildbot
+kbase=/opt/kernel
 
 for system in saturn.roeck-us.net desktop jupiter mars hyperion titan
 do
@@ -28,3 +29,18 @@ do
 		echo "done"
 	fi
 done
+
+if [ -d /opt/buildbot/virtual/disk ]
+then
+	echo -n "virtual: "
+	rsync -l --delete -r ${base}/bin ${base}/rootfs ${base}/share \
+		${base}/lib ${base}/include ${base}/kconfig ${base}/qemu-install \
+		/opt/buildbot/virtual/disk/${base}
+	result=$?
+	if [ ${result} -ne 0 ]
+	then
+		echo "failed"
+	else
+		echo "done"
+	fi
+fi
