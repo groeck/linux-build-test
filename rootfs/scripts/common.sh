f57f29ddc89b6d025d3a527c8edb7b3a9d247701
Guenter Roeck <linux@roeck-us.net>
2017-01-11 10:42:24 -0800

    rootfs: common: Improve crash detection
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/scripts/common.sh
+++ rootfs/scripts/common.sh
@@ -222,7 +222,7 @@ dowait()
 	    fi
 	fi
 
-	egrep "^Oops:|Kernel panic|Internal error:" ${logfile} >/dev/null 2>&1
+	egrep "Oops: |Kernel panic|Internal error:" ${logfile} >/dev/null 2>&1
 	if [ $? -eq 0 ]
 	then
 	    # x86 has the habit of crashing in restart once in a while.
