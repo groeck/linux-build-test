b6838bcaa0ceb8f5e566415a15c0aa7a583130e8
Guenter Roeck <linux@roeck-us.net>
2017-09-13 11:42:53 -0700

    stable-queue-sync: Add 4.13, improve stability
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/stable-queue-sync.sh
+++ bin/stable-queue-sync.sh
@@ -10,13 +10,14 @@ if [ $# -gt 0 ]
 then
 	releases=($*)
 else
-	releases=(3.18 4.4 4.9 4.12)
+	releases=(3.18 4.4 4.9 4.12 4.13)
 fi
 
 do_import()
 {
 	release=$1
 	target=linux-${release}.y.queue
+	source_s=""
 	case "${release}" in
 	"3.12")
 		source=origin/stable-${release}-queue
@@ -26,6 +27,7 @@ do_import()
 		;;
 	*)
 		source=origin/linux-${release}.y
+		source_s=stable/linux-${release}.y
 		;;
 	esac
 
@@ -33,28 +35,39 @@ do_import()
 	echo source: ${source} target: ${target}
 
 	# Add local repository to remote only if needed
-	git remote | grep local || {
+	git remote | grep -q local || {
 		git remote add local git://server.roeck-us.net/git/linux-stable.git
 		git config remote.local.pushurl "ssh://git@server.roeck-us.net//var/cache/git/linux-stable.git"
 	}
 	# Also add -stable if needed
-	# git remote | grep stable || {
+	# git remote | grep -q stable || {
 	# 	git remote add stable git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 	# }
 
 	git fetch --all
 	# Check if source branch exists. If not, there is nothing we can do.
-	git branch -r | grep ${source}  >/dev/null 2>&1
+	git branch -r | grep -q ${source}  >/dev/null 2>&1
 	if [ $? -ne 0 ]
 	then
-		echo "Source branch does not exist: Aborting."
-		return 1
+		if [ -z ${source_s} ]
+		then
+			echo "Source branch does not exist, aborting."
+			return 1
+		fi
+		echo "Source branch does not exist in ${source}, checking ${source_s}."
+		git branch -r | grep -q ${source_s}  >/dev/null 2>&1
+		if [ $? -ne 0 ]
+		then
+			echo "Source branch does not exist, aborting."
+			return 1
+		fi
+		source=${source_s}
 	fi
 	# Check if target branch exists
 	# If not, we have to create it first
 	# Note: "git push local ${source}:${target}" does not work
 	# if ${target} does not exist.
-	git branch -r | grep local/${target} >/dev/null 2>&1
+	git branch -r | grep -q local/${target} >/dev/null 2>&1
 	if [ $? -ne 0 ]
 	then
 		git checkout -b ${target} ${source}
