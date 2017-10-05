e725b81e8ea6f36180c86333c0c599ccfe5c5854
Guenter Roeck <linux@roeck-us.net>
2017-10-05 07:03:02 -0700

    cleanup: Split into multiple parts
    Command will fail otherwise if there are too many files to remove.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,4 +1,6 @@
 # Remove all log files older than 40 days
 
 cd /opt/buildbot/master
+rm $(find master-* next-* -type f -ctime +40 | grep -v workdir) 2>/dev/null
+rm $(find stable-updates stable-queue-* -type f -ctime +40 | grep -v workdir) 2>/dev/null
 rm $(find mmotm-* qemu-* stable-updates stable-queue-* hwmon* master-* next-* -type f -ctime +40 | grep -v workdir)
