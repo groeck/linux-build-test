32c468125530b8b57d06d9559987853c5011d432
Guenter Roeck <linux@roeck-us.net>
2018-02-25 09:00:53 -0800

    install-gcc: Improve parameter validation
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/install-gcc.sh
+++ bin/install-gcc.sh
@@ -1,9 +1,14 @@
-if [ -e /tmp/$1 ]; then
+if [ -z "$1" ]; then
+    echo "Need parameter"
+    exit 1
+fi
+
+if [ ! -f /tmp/$1 ]; then
     f=/tmp/$1
-elif [ -e $1 ]; then
+elif [ -f $1 ]; then
     f=$1
 else
-    echo "$1 not found"
+    echo "$1 does not exist or is not a file"
     exit 1
 fi
 
