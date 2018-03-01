c096c52d862dfe8376099546b653fdab68f8db81
Guenter Roeck <linux@roeck-us.net>
2018-03-01 14:42:56 -0800

    rootfs: s390: Add test with root file system
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/s390/run-qemu-s390.sh
+++ rootfs/s390/run-qemu-s390.sh
@@ -7,7 +7,6 @@ dir=$(cd $(dirname $0); pwd)
 QEMU=${QEMU:-${QEMU_BIN}/qemu-system-s390x}
 PREFIX=s390-linux-
 ARCH=s390
-rootfs=busybox-s390.cpio
 PATH_SH=/opt/kernel/s390/gcc-6.4.0/bin
 
 PATH=${PATH_SH}:${PATH}
@@ -17,6 +16,11 @@ patch_defconfig()
     local defconfig=$1
     local fixup=$2
 
+    # make sure DEVTMPFS is enabled and auto-mounts
+    sed -i -e '/CONFIG_DEVTMPFS/d' ${defconfig}
+    echo "CONFIG_DEVTMPFS=y" >> ${defconfig}
+    echo "CONFIG_DEVTMPFS_MOUNT=y" >> ${defconfig}
+
     # qemu only supports MARCH_Z900. Older kernels select it as default,
     # but newer kernels may select MARCH_Z196.
     sed -i -e '/CONFIG_MARCH_Z/d' ${defconfig}
@@ -24,9 +28,12 @@ patch_defconfig()
     echo "CONFIG_MARCH_Z900=y" >> ${defconfig}
 }
 
+cached_config=""
+
 runkernel()
 {
     local defconfig=$1
+    local rootfs=$2
     local pid
     local retcode
     local logfile=/tmp/runkernel-$$.log
@@ -34,17 +41,34 @@ runkernel()
 
     echo -n "Building ${ARCH}:${defconfig} ... "
 
-    dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig} "" fixup
-    if [ $? -ne 0 ]
-    then
+    if [ "${cached_config}" != "${defconfig}" ]; then
+	dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig} "" fixup
+	if [ $? -ne 0 ]; then
+	    return 1
+	fi
+	cached_config="${defconfig}"
+    else
+	setup_rootfs ${rootfs} ""
+    fi
+
+    if [ $? -ne 0 ]; then
 	return 1
     fi
 
     echo -n "running ..."
 
+    if [[ "${rootfs}" == *cpio ]]; then
+	initcli="rdinit=/sbin/init"
+	diskcmd="-initrd ${rootfs}"
+    else
+	initcli="root=/dev/vda rw"
+	diskcmd="-drive file=${rootfs},format=raw,if=none,id=d0 \
+		-device virtio-blk-ccw,devno=fe.0.0001,drive=d0"
+    fi
+
     ${QEMU} -kernel vmlinux \
-        -initrd ${rootfs} \
-	-append "rdinit=/sbin/init doreboot" \
+        ${diskcmd} \
+	-append "${initcli} doreboot" \
 	-m 512 \
 	-nographic -monitor null --no-reboot > ${logfile} 2>&1 &
 
@@ -58,7 +82,9 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel defconfig
+runkernel defconfig busybox-s390.cpio
 retcode=$?
+runkernel defconfig rootfs.s390.ext2
+retcode=$((${retcode} + $?))
 
 exit ${retcode}
