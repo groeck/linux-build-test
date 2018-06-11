625cad3a17687928d52d2c75ee404db8c0f8fc85
Guenter Roeck <linux@roeck-us.net>
2018-06-11 10:12:37 -0700

    rootfs: x86_64: Switch to use defconfig, and test initrd
    defconfig is more appropriate than a private configuration, so start
    using it and drop the private configuration files Also add initrd
    based tests.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/x86_64/run-qemu-x86_64.sh
+++ rootfs/x86_64/run-qemu-x86_64.sh
@@ -13,8 +13,8 @@ ARCH=x86_64
 
 # Older releases don't like gcc 6+
 rel=$(git describe | cut -f1 -d- | cut -f1,2 -d.)
-case ${rel} in
-v3.16|v3.18)
+case "${rel}" in
+"v3.16"|"v3.18")
 	PATH_X86=/opt/poky/1.3/sysroots/x86_64-pokysdk-linux/usr/bin/x86_64-poky-linux
 	PREFIX="x86_64-poky-linux-"
 	;;
@@ -26,20 +26,38 @@ esac
 
 PATH=${PATH_X86}:${PATH}
 
-cached_defconfig=""
+cached_config=""
+
+patch_defconfig()
+{
+    local defconfig=$1
+    local fixup=$2
+
+    if [[ "${fixup}" = "nosmp" ]]; then
+	sed -i -e '/CONFIG_SMP/d' ${defconfig}
+    fi
+}
 
 runkernel()
 {
     local defconfig=$1
-    local cpu=$2
-    local mach=$3
+    local fixup=$2
+    local cpu=$3
+    local mach=$4
+    local rootfs=$5
     local drive
     local pid
     local retcode
-    local rootfs=rootfs.ext2
     local logfile=/tmp/runkernel-$$.log
     local waitlist=("machine restart" "Restarting" "Boot successful" "Rebooting")
-    local pbuild="${ARCH}:${mach}:${cpu}:${defconfig}"
+    local pbuild="${ARCH}:${mach}:${cpu}:${defconfig}:${fixup}"
+    local config="${defconfig}:${fixup}"
+
+    if [[ "${rootfs}" == *cpio ]]; then
+	pbuild+=":initrd"
+    else
+	pbuild+=":rootfs"
+    fi
 
     if [ -n "${machine}" -a "${machine}" != "${mach}" ]
     then
@@ -55,32 +73,26 @@ runkernel()
 
     echo -n "Building ${pbuild} ... "
 
-    if [ "${cached_defconfig}" != "${defconfig}" ]
+    if [ "${cached_config}" != "${config}" ]
     then
-	dosetup ${ARCH} ${PREFIX} "" ${rootfs} ${defconfig}
-	if [ $? -ne 0 ]
-	then
+	dosetup "${ARCH}" "${PREFIX}" "" "${rootfs}" "${defconfig}" "" "${fixup}"
+	if [ $? -ne 0 ]; then
 	    return 1
 	fi
-	cached_defconfig=${defconfig}
+	cached_config="${config}"
+    else
+	setup_rootfs "${rootfs}" ""
     fi
 
     echo -n "running ..."
 
-    case "${mach}" in
-    pc)
-	drive=hda
-	usb="-usb -device usb-wacom-tablet"
-	;;
-    q35)
-	drive=sda
-	usb="-usb -device usb-wacom-tablet"
-	;;
-    *)
-        echo "failed (unsupported machine type ${mach})"
-	return 1
-	;;
-    esac
+    if [[ "${rootfs}" == *cpio ]]; then
+	initcli="rdinit=/sbin/init"
+	diskcmd="-initrd ${rootfs}"
+    else
+	initcli="root=/dev/sda rw"
+	diskcmd="-drive file=${rootfs},if=ide,format=raw"
+    fi
 
     kvm=""
     mem="-m 256"
@@ -91,9 +103,9 @@ runkernel()
     fi
 
     ${QEMU} -kernel arch/x86/boot/bzImage \
-	-M ${mach} -cpu ${cpu} ${kvm} ${usb} -no-reboot ${mem} \
-	-drive file=${rootfs},format=raw,if=ide \
-	--append "root=/dev/${drive} rw console=ttyS0 console=tty doreboot" \
+	-M ${mach} -cpu ${cpu} ${kvm} -usb -no-reboot ${mem} \
+	${diskcmd} \
+	--append "${initcli} console=ttyS0 console=tty doreboot" \
 	-nographic > ${logfile} 2>&1 &
 
     pid=$!
@@ -108,27 +120,35 @@ echo
 
 retcode=0
 
-# runkernel qemu_x86_64_pc_defconfig kvm64 q35
+# runkernel defconfig kvm64 q35
 # retcode=$((${retcode} + $?))
-runkernel qemu_x86_64_pc_defconfig Broadwell-noTSX q35
+runkernel defconfig smp Broadwell-noTSX q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel qemu_x86_64_pc_defconfig IvyBridge q35
+runkernel defconfig smp IvyBridge q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel qemu_x86_64_pc_defconfig SandyBridge q35
+runkernel defconfig smp SandyBridge q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel qemu_x86_64_pc_defconfig Haswell q35
+runkernel defconfig smp Haswell q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel qemu_x86_64_pc_defconfig core2duo pc
+runkernel defconfig smp core2duo pc rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel qemu_x86_64_pc_defconfig Nehalem q35
+runkernel defconfig smp Nehalem q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel qemu_x86_64_pc_defconfig phenom pc
+runkernel defconfig smp phenom pc rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel qemu_x86_64_pc_defconfig Opteron_G1 q35
+runkernel defconfig smp Opteron_G1 q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel qemu_x86_64_pc_nosmp_defconfig Opteron_G4 pc
+runkernel defconfig smp Opteron_G5 q35 rootfs.cpio
 retcode=$((${retcode} + $?))
-runkernel qemu_x86_64_pc_nosmp_defconfig IvyBridge q35
+runkernel defconfig smp EPYC-IBPB q35 rootfs.cpio
+retcode=$((${retcode} + $?))
+runkernel defconfig smp Skylake-Client q35 rootfs.cpio
+retcode=$((${retcode} + $?))
+runkernel defconfig smp Skylake-Server q35 rootfs.cpio
+retcode=$((${retcode} + $?))
+runkernel defconfig nosmp Opteron_G4 pc rootfs.ext2
+retcode=$((${retcode} + $?))
+runkernel defconfig nosmp IvyBridge q35 rootfs.ext2
 retcode=$((${retcode} + $?))
 
 exit ${retcode}
