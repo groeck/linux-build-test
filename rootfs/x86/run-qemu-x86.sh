97480584b08d5c76ee8c1fe0ec81861ef7f6eab8
Guenter Roeck <linux@roeck-us.net>
2018-06-11 06:38:02 -0700

    rootfs: x86: Switch to self-generated rootfs
    The one we used so far is old and has random restart failures.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/x86/run-qemu-x86.sh
+++ rootfs/x86/run-qemu-x86.sh
@@ -36,7 +36,7 @@ runkernel()
     local drive
     local pid
     local retcode
-    local rootfs=core-image-minimal-qemux86.ext3
+    local rootfs=rootfs.ext2
     local logfile=/tmp/runkernel-$$.log
     local waitlist=("machine restart" "Restarting" "Boot successful" "Rebooting")
     local build="${ARCH}:${cpu}:${mach}:${defconfig}"
