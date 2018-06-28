4a89027eef67ab0997ae9dc22866e9d059033144
Guenter Roeck <linux@roeck-us.net>
2018-06-27 22:16:00 -0700

    rootfs: microblazeel: Add petalogix-ml605
    Also provide command line and initramfs via command line and no longer
    build into kernel. Use new root file system built with buildroot which
    auto-detects the console.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/microblazeel/run-qemu-microblazeel.sh
+++ rootfs/microblazeel/run-qemu-microblazeel.sh
@@ -7,7 +7,6 @@ dir=$(cd $(dirname $0); pwd)
 QEMU=${QEMU:-${QEMU_BIN}/qemu-system-microblazeel}
 PREFIX=microblazeel-linux-
 ARCH=microblaze
-rootfs=busybox-microblazeel.cpio
 PATH_MICROBLAZE=/opt/kernel/microblazeel/gcc-4.9.1/usr/bin
 
 PATH=${PATH_MICROBLAZE}:${PATH}
@@ -15,27 +14,42 @@ PATH=${PATH_MICROBLAZE}:${PATH}
 runkernel()
 {
     local defconfig=$1
+    local mach=$2
+    local rootfs=$3
     local pid
     local retcode
     local waitlist=("Machine restart" "Boot successful" "Rebooting")
     local logfile=/tmp/runkernel-$$.log
 
-    echo -n "Building ${ARCH}:${defconfig} ... "
+    echo -n "Building ${ARCH}:${mach}:${defconfig} ... "
 
     dosetup -d "${rootfs}" "${defconfig}"
-    if [ $? -ne 0 ]
-    then
+    if [ $? -ne 0 ]; then
 	return 1
     fi
 
     echo -n "running ..."
 
-    ${QEMU} -M petalogix-s3adsp1800 \
-	-kernel arch/microblaze/boot/linux.bin -no-reboot -nographic \
-	> ${logfile} 2>&1 &
-
-    pid=$!
-
+    case "${mach}" in
+    "petalogix-s3adsp1800")
+	${QEMU} -M petalogix-s3adsp1800 \
+		-kernel arch/microblaze/boot/linux.bin -no-reboot \
+		-initrd ${rootfs} \
+		-append "rdinit=/sbin/init console=ttyUL0,115200" \
+		-monitor none -nographic \
+		> ${logfile} 2>&1 &
+	pid=$!
+	;;
+    "petalogix-ml605")
+	${QEMU} -M petalogix-ml605 -m 256 \
+		-kernel arch/microblaze/boot/linux.bin -no-reboot \
+		-initrd ${rootfs} \
+		-append "rdinit=/sbin/init console=ttyS0,115200" \
+		-monitor none -serial stdio -nographic \
+		> ${logfile} 2>&1 &
+	pid=$!
+	;;
+    esac
     dowait ${pid} ${logfile} manual waitlist[@]
     retcode=$?
     rm -f ${logfile}
@@ -45,6 +59,10 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel qemu_microblazeel_defconfig
+retcode=0
+runkernel qemu_microblazeel_defconfig petalogix-s3adsp1800 rootfs.cpio
+retcode=$((retcode + $?))
+runkernel qemu_microblazeel_ml605_defconfig petalogix-ml605 rootfs.cpio
+retcode=$((retcode + $?))
 
 exit $?
