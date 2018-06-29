f371825a73d22a508788ca40aacebde4c35cd7ba
Guenter Roeck <linux@roeck-us.net>
2018-06-28 22:28:30 -0700

    rootfs: parisc: Add initrd test
    Also move standard image from squashfs (unusual) to ext2,
    and compress all file system images.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/parisc/run-qemu-parisc.sh
+++ rootfs/parisc/run-qemu-parisc.sh
@@ -8,41 +8,58 @@ QEMU=${QEMU:-${QEMU_BIN}/qemu-system-hppa}
 
 PREFIX=hppa-linux-
 ARCH=parisc
-rootfs=rootfs.squashfs
 
 rel=$(git describe | cut -f1 -d- | cut -f1,2 -d.)
 PATH_PARISC=/opt/kernel/hppa/gcc-7.3.0/bin
 PATH=${PATH}:${PATH_PARISC}
 
-patch_defconfig()
-{
-    local defconfig=$1
-    # enable squashfs
-    sed -i -e '/CONFIG_SQUASHFS/d' ${defconfig}
-    echo "CONFIG_SQUASHFS=y" >>${defconfig}
-}
+cached_config=""
 
 runkernel()
 {
     local defconfig=$1
+    local rootfs=$2
     local pid
     local retcode
     local logfile=/tmp/runkernel-$$.log
     local waitlist=("reboot: Restarting system" "Boot successful" "Requesting system reboot")
+    local build="${ARCH}:${defconfig}"
+    local initcli
+    local diskcmd
 
-    echo -n "Building ${ARCH}:${defconfig} ... "
+    if [[ "${rootfs%.gz}" == *cpio ]]; then
+	build+=":initrd"
+	initcli="rdinit=/sbin/init"
+	diskcmd="-initrd ${rootfs%.gz}"
+    else
+	build+=":rootfs"
+	initcli="root=/dev/sda"
+	diskcmd="-drive file=${rootfs%.gz},format=raw,if=scsi"
+    fi
 
-    dosetup -f fixup "${rootfs}" "${defconfig}"
-    if [ $? -ne 0 ]
-    then
-	return 1
+    echo -n "Building ${build} ... "
+
+    if [ "${cached_config}" != "${defconfig}" ]; then
+	# dosetup -f fixup "${rootfs}" "${defconfig}"
+	dosetup "${rootfs}" "${defconfig}"
+	if [ $? -ne 0 ]; then
+	    return 1
+	fi
+	cached_config="${defconfig}"
+    else
+	setup_rootfs "${rootfs}"
+    fi
+
+    if [[ "${rootfs}" == *.gz ]]; then
+	gunzip -f "${rootfs}"
+	rootfs="${rootfs%.gz}"
     fi
 
     echo -n "running ..."
 
     ${QEMU} -kernel vmlinux -no-reboot \
-	-drive file=${rootfs},format=raw,if=scsi \
-	-append "root=/dev/sda console=ttyS0,115200" \
+	${diskcmd} \
+	-append "${initcli} console=ttyS0,115200" \
 	-nographic -monitor null > ${logfile} 2>&1 &
 
     pid=$!
@@ -55,7 +72,10 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel defconfig
-retcode=$?
+retcode=0
+runkernel defconfig rootfs.cpio.gz
+retcode=$((retcode + $?))
+runkernel defconfig rootfs.ext2.gz
+retcode=$((retcode + $?))
 
 exit ${retcode}
