b1e8c45f5225b13f9d1877a044f3ffe95aabcade
Guenter Roeck <linux@roeck-us.net>
2018-07-30 22:33:27 -0700

    rootfs: x86_64: Add NVME, SCSI, and USB boot tests
    Add NVME, SCSI, and USB drive boot tests. Also enable standard
    debug options (-d, -n). Exclude SCSI boot tests from older kernels
    as needed.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/x86_64/run-qemu-x86_64.sh
+++ rootfs/x86_64/run-qemu-x86_64.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-machine=$1
-cputype=$2
-
-
 dir=$(cd $(dirname $0); pwd)
 . ${dir}/../scripts/config.sh
 . ${dir}/../scripts/common.sh
+
+parse_args "$@"
+shift $((OPTIND - 1))
+
+machine=$1
+cputype=$2
 
 QEMU=${QEMU:-${QEMU_BIN}/qemu-system-x86_64}
 ARCH=x86_64
@@ -28,6 +30,16 @@ PATH=${PATH_X86}:${PATH}
 
 cached_config=""
 
+skip_316="defconfig:smp:scsi[AM53C974] \
+	defconfig:smp:scsi[DC395] \
+	defconfig:nosmp:scsi[AM53C974] \
+	defconfig:nosmp:scsi[DC395]"
+
+skip_318="defconfig:smp:scsi[AM53C974] \
+	defconfig:smp:scsi[DC395] \
+	defconfig:nosmp:scsi[AM53C974] \
+	defconfig:nosmp:scsi[DC395]"
+
 patch_defconfig()
 {
     local defconfig=$1
@@ -36,6 +48,12 @@ patch_defconfig()
     if [[ "${fixup}" = "nosmp" ]]; then
 	sed -i -e '/CONFIG_SMP/d' ${defconfig}
     fi
+    # Always enable SCSI controller drivers and NVME
+    echo "CONFIG_BLK_DEV_NVME=y" >> ${defconfig}
+    echo "CONFIG_SCSI_LOWLEVEL=y" >> ${defconfig}
+    echo "CONFIG_SCSI_DC395x=y" >> ${defconfig}
+    echo "CONFIG_SCSI_AM53C974=y" >> ${defconfig}
+    echo "CONFIG_MEGARAID_SAS=y" >> ${defconfig}
 }
 
 runkernel()
@@ -51,7 +69,8 @@ runkernel()
     local logfile=/tmp/runkernel-$$.log
     local waitlist=("machine restart" "Restarting" "Boot successful" "Rebooting")
     local pbuild="${ARCH}:${mach}:${cpu}:${defconfig}:${fixup}"
-    local config="${defconfig}:${fixup}"
+    local build="${defconfig}:${fixup}"
+    local config="${defconfig}:${fixup%:*}"
 
     if [[ "${rootfs}" == *cpio ]]; then
 	pbuild+=":initrd"
@@ -73,6 +92,10 @@ runkernel()
 
     echo -n "Building ${pbuild} ... "
 
+    if ! checkskip "${build}" ; then
+	return 0
+    fi
+
     if [ "${cached_config}" != "${config}" ]
     then
 	dosetup -f "${fixup}" "${rootfs}" "${defconfig}"
@@ -91,7 +114,31 @@ runkernel()
 	diskcmd="-initrd ${rootfs}"
     else
 	initcli="root=/dev/sda rw"
-	diskcmd="-drive file=${rootfs},if=ide,format=raw"
+	if [[ "${fixup}" == *sata ]]; then
+	    diskcmd="-drive file=${rootfs},if=ide,format=raw"
+	elif [[ "${fixup}" == *nvme ]]; then
+	    initcli="root=/dev/nvme0n1 rw"
+	    diskcmd="-device nvme,serial=foo,drive=d0 \
+		-drive file=${rootfs},if=none,format=raw,id=d0"
+	elif [[ "${fixup}" == *usbdisk ]]; then
+	    initcli="root=/dev/sda rw rootwait"
+	    diskcmd="-device usb-storage,drive=d0 \
+		-drive file=${rootfs},if=none,format=raw,id=d0"
+	elif [[ "${fixup##*:}" == scsi* ]]; then
+	    case "${fixup##*:}" in
+	    "scsi[DC395]")
+		device="dc390"
+		;;
+	    "scsi[AM53C974]")
+		device="am53c974"
+		;;
+	    "scsi[MEGASAS]")
+		device="megasas-gen2"
+		;;
+	    esac
+	    diskcmd="-device "${device}" -device scsi-hd,drive=d0 \
+		-drive file=${rootfs},if=none,format=raw,id=d0"
+	fi
     fi
 
     kvm=""
@@ -102,13 +149,17 @@ runkernel()
 	mem="-m 1024"
     fi
 
+    [[ ${dodebug} -ne 0 ]] && set -x
+
     ${QEMU} -kernel arch/x86/boot/bzImage \
 	-M ${mach} -cpu ${cpu} ${kvm} -usb -no-reboot ${mem} \
 	${diskcmd} \
 	--append "earlycon=uart8250,io,0x3f8,9600n8 ${initcli} console=ttyS0 console=tty doreboot" \
 	-nographic > ${logfile} 2>&1 &
-
     pid=$!
+
+    [[ ${dodebug} -ne 0 ]] && set +x
+
     dowait ${pid} ${logfile} manual waitlist[@]
     retcode=$?
     rm -f ${logfile}
@@ -122,35 +173,35 @@ retcode=0
 
 # runkernel defconfig kvm64 q35
 # retcode=$((${retcode} + $?))
-runkernel defconfig smp Broadwell-noTSX q35 rootfs.ext2
+runkernel defconfig smp:sata Broadwell-noTSX q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig smp IvyBridge q35 rootfs.ext2
+runkernel defconfig smp:nvme IvyBridge q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig smp SandyBridge q35 rootfs.ext2
+runkernel defconfig smp:scsi[DC395] SandyBridge q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig smp Haswell q35 rootfs.ext2
+runkernel defconfig smp:scsi[AM53C974] Haswell q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig smp core2duo pc rootfs.ext2
+runkernel defconfig smp:scsi[MEGASAS] core2duo pc rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig smp Nehalem q35 rootfs.ext2
+runkernel defconfig smp:nvme Nehalem q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig smp phenom pc rootfs.ext2
+runkernel defconfig smp:sata phenom pc rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig smp Opteron_G1 q35 rootfs.ext2
+runkernel defconfig smp:scsi[DC395] Opteron_G1 q35 rootfs.ext2
 retcode=$((${retcode} + $?))
 runkernel defconfig smp Opteron_G5 q35 rootfs.cpio
 retcode=$((${retcode} + $?))
-runkernel defconfig smp EPYC-IBPB q35 rootfs.cpio
+runkernel defconfig smp:usbdisk EPYC-IBPB q35 rootfs.ext2
 retcode=$((${retcode} + $?))
 runkernel defconfig smp Skylake-Client q35 rootfs.cpio
 retcode=$((${retcode} + $?))
-runkernel defconfig smp Skylake-Server q35 rootfs.ext2
+runkernel defconfig smp:usbdisk Skylake-Server q35 rootfs.ext2
 retcode=$((${retcode} + $?))
 runkernel defconfig smp Opteron_G3 q35 rootfs.cpio
 retcode=$((${retcode} + $?))
-runkernel defconfig nosmp Opteron_G4 pc rootfs.ext2
+runkernel defconfig nosmp:scsi[AM53C974] Opteron_G4 pc rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig nosmp IvyBridge q35 rootfs.ext2
+runkernel defconfig nosmp:scsi[MEGASAS] IvyBridge q35 rootfs.ext2
 retcode=$((${retcode} + $?))
 
 exit ${retcode}
