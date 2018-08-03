0236389d3fab6a7c30ff9c437dd7310d5e0f9300
Guenter Roeck <linux@roeck-us.net>
2018-08-02 18:45:08 -0700

    rootfs: mips64: Additional boot tests
    Add various additional boot tests, including initrd. Test both N32 and
    N64 ABI. Update root file system to new version.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/mips64/run-qemu-mips64.sh
+++ rootfs/mips64/run-qemu-mips64.sh
@@ -18,7 +18,6 @@ PREFIX=mips-linux-
 cpu="-cpu 5KEc"
 
 # machine specific information
-rootfs=core-image-minimal-qemumips64.ext3
 ARCH=mips
 KERNEL_IMAGE=vmlinux
 QEMU_MACH=malta
@@ -37,15 +36,45 @@ patch_defconfig()
     echo "CONFIG_DEVTMPFS=y" >> ${defconfig}
     echo "CONFIG_DEVTMPFS_MOUNT=y" >> ${defconfig}
 
+    echo "CONFIG_BLK_DEV_INITRD=y" >> ${defconfig}
+
     # 64 bit build
     echo "CONFIG_32BIT=n" >> ${defconfig}
     echo "CONFIG_CPU_MIPS32_R1=n" >> ${defconfig}
     echo "CONFIG_CPU_MIPS64_R1=y" >> ${defconfig}
     echo "CONFIG_64BIT=y" >> ${defconfig}
 
+    # Support N32 and O32 binaries
+    echo "CONFIG_MIPS32_O32=y" >> ${defconfig}
+    echo "CONFIG_MIPS32_N32=y" >> ${defconfig}
+
     # Build a big endian image
     echo "CONFIG_CPU_LITTLE_ENDIAN=n" >> ${defconfig}
     echo "CONFIG_CPU_BIG_ENDIAN=y" >> ${defconfig}
+
+    # MMC/SDHCI
+    echo "CONFIG_MMC=y" >> ${defconfig}
+    echo "CONFIG_MMC_SDHCI=y" >> ${defconfig}
+    echo "CONFIG_MMC_SDHCI_PCI=y" >> ${defconfig}
+
+    # SCSI
+    echo "CONFIG_SCSI=y" >> ${defconfig}
+    echo "CONFIG_BLK_DEV_SD=y" >> ${defconfig}
+    echo "CONFIG_SCSI_DC395x=y" >> ${defconfig}
+    echo "CONFIG_SCSI_AM53C974=y" >> ${defconfig}
+    echo "CONFIG_MEGARAID_SAS=y" >> ${defconfig}
+    echo "CONFIG_FUSION=y" >> ${defconfig}
+    echo "CONFIG_FUSION_SAS=y" >> ${defconfig}
+    echo "CONFIG_SCSI_SYM53C8XX_2=y" >> ${defconfig}
+
+    # NVME
+    echo "CONFIG_BLK_DEV_NVME=y" >> ${defconfig}
+
+    # USB
+    echo "CONFIG_USB=y" >> ${defconfig}
+    echo "CONFIG_USB_XHCI_HCD=y" >> ${defconfig}
+    echo "CONFIG_USB_STORAGE=y" >> ${defconfig}
+    echo "CONFIG_USB_UAS=y" >> ${defconfig}
 
     for fixup in ${fixups}; do
 	if [[ "${fixup}" == "smp" ]]; then
@@ -65,13 +94,19 @@ runkernel()
 {
     local defconfig=$1
     local fixup=$2
+    local rootfs=$3
     local pid
     local retcode
-    local initcli="root=/dev/sda rw"
-    local diskcmd="-drive file=${rootfs},format=raw,if=ide"
     local logfile=/tmp/runkernel-$$.log
     local waitlist=("Boot successful" "Rebooting")
-    local build="mips64:${defconfig}:${fixup}"
+    local build="mips64:${defconfig}"
+
+    if [[ "${rootfs}" == *.cpio* ]]; then
+	build+=":initrd"
+    else
+	build+=":${fixup}"
+	build+=":rootfs"
+    fi
 
     if [ -n "${config}" -a "${config}" != "${defconfig}" ]
     then
@@ -96,6 +131,8 @@ runkernel()
 	setup_rootfs "${rootfs}"
     fi
 
+    rootfs="${rootfs%.gz}"
+
     echo -n "running ..."
 
     if ! common_diskcmd "${fixup##*:}" "${rootfs}"; then
@@ -108,7 +145,7 @@ runkernel()
 	${cpu} \
 	${diskcmd} \
 	-vga cirrus -no-reboot -m 128 \
-	--append "${initcli} mem=128M console=ttyS0 console=tty ${extracli} doreboot" \
+	--append "${initcli} mem=128M console=ttyS0 console=tty ${extracli}" \
 	-nographic > ${logfile} 2>&1 &
     pid=$!
 
@@ -123,9 +160,48 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel malta_defconfig nosmp:ata
-retcode=$?
-runkernel malta_defconfig smp:ata
+retcode=0
+
+runkernel malta_defconfig smp:ata rootfs-n32.cpio.gz
+retcode=$((retcode + $?))
+runkernel malta_defconfig smp:ata rootfs-n32.ext2.gz
+retcode=$((retcode + $?))
+runkernel malta_defconfig smp:mmc rootfs-n64.ext2.gz
+retcode=$((retcode + $?))
+
+if [[ ${runall} -ne 0 ]]; then
+    # QID timeout, hang
+    runkernel malta_defconfig smp:nvme rootfs-n32.ext2.gz
+    retcode=$((retcode + $?))
+fi
+
+runkernel malta_defconfig smp:usb rootfs-n32.ext2.gz
+retcode=$((retcode + $?))
+runkernel malta_defconfig smp:usb-uas rootfs-n64.ext2.gz
+retcode=$((retcode + $?))
+runkernel malta_defconfig smp:scsi[53C810] rootfs-n32.ext2.gz
+retcode=$((retcode + $?))
+
+if [[ ${runall} -ne 0 ]]; then
+    # sym0: interrupted SCRIPT address not found
+    runkernel malta_defconfig smp:scsi[53C895A] rootfs-n32.ext2.gz
+    retcode=$((retcode + $?))
+fi
+
+runkernel malta_defconfig smp:scsi[DC395] rootfs-n64.ext2.gz
+retcode=$((retcode + $?))
+runkernel malta_defconfig smp:scsi[AM53C974] rootfs-n32.ext2.gz
+retcode=$((retcode + $?))
+runkernel malta_defconfig smp:scsi[MEGASAS] rootfs-n64.ext2.gz
+retcode=$((retcode + $?))
+runkernel malta_defconfig smp:scsi[MEGASAS2] rootfs-n32.ext2.gz
+retcode=$((retcode + $?))
+runkernel malta_defconfig smp:scsi[FUSION] rootfs-n64.ext2.gz
+retcode=$((retcode + $?))
+
+runkernel malta_defconfig nosmp:ata rootfs-n32.ext2.gz
+retcode=$((retcode + $?))
+runkernel malta_defconfig nosmp:mmc rootfs-n64.ext2.gz
 retcode=$((retcode + $?))
 
 exit ${retcode}
