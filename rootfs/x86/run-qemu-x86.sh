9f2f294aff669a9f175466c08dd726c0b4029d74
Guenter Roeck <linux@roeck-us.net>
2018-08-11 20:04:01 -0700

    rootfs: x86: Modernize
    Pretty much copy from x86_64. Use infras as much as possible,
    add new build tests.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/x86/run-qemu-x86.sh
+++ rootfs/x86/run-qemu-x86.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-_cpu=$1
-_mach=$2
-_defconfig=$3
-
 dir=$(cd $(dirname $0); pwd)
 . ${dir}/../scripts/config.sh
 . ${dir}/../scripts/common.sh
+
+parse_args "$@"
+shift $((OPTIND - 1))
+
+_cpu=$1
+_mach=$2
+_defconfig=$3
 
 QEMU=${QEMU:-${QEMU_BIN}/qemu-system-i386}
 ARCH=i386
@@ -32,8 +35,26 @@ patch_defconfig()
     local fixup=$2
 
     if [[ "${fixup}" = "nosmp" ]]; then
-	sed -i -e '/CONFIG_SMP/d' ${defconfig}
+	echo "CONFIG_SMP=n" >> ${defconfig}
     fi
+
+    # Always enable SCSI controller drivers and NVME
+    echo "CONFIG_BLK_DEV_NVME=y" >> ${defconfig}
+    echo "CONFIG_SCSI_LOWLEVEL=y" >> ${defconfig}
+    echo "CONFIG_SCSI_DC395x=y" >> ${defconfig}
+    echo "CONFIG_SCSI_AM53C974=y" >> ${defconfig}
+    echo "CONFIG_SCSI_SYM53C8XX_2=y" >> ${defconfig}
+    echo "CONFIG_MEGARAID_SAS=y" >> ${defconfig}
+    echo "CONFIG_FUSION=y" >> ${defconfig}
+    echo "CONFIG_FUSION_SAS=y" >> ${defconfig}
+
+    # Always enable MMC/SDHCI support
+    echo "CONFIG_MMC=y" >> ${defconfig}
+    echo "CONFIG_MMC_SDHCI=y" >> ${defconfig}
+    echo "CONFIG_MMC_SDHCI_PCI=y" >> ${defconfig}
+
+    # Enable USB-UAS (USB Attached SCSI)
+    echo "CONFIG_USB_UAS=y" >> ${defconfig}
 }
 
 runkernel()
@@ -48,13 +69,14 @@ runkernel()
     local retcode
     local logfile=/tmp/runkernel-$$.log
     local waitlist=("machine restart" "Restarting" "Boot successful" "Rebooting")
-    local build="${ARCH}:${cpu}:${mach}:${defconfig}:${fixup}"
-    local config="${defconfig}:${fixup}"
+    local pbuild="${ARCH}:${mach}:${cpu}:${defconfig}:${fixup}"
+    local build="${defconfig}:${fixup}"
+    local config="${defconfig}:${fixup%:*}"
 
     if [[ "${rootfs}" == *cpio ]]; then
-	build+=":initrd"
+	pbuild+=":initrd"
     else
-	build+=":rootfs"
+	pbuild+=":rootfs"
     fi
 
     if [ -n "${_cpu}" -a "${_cpu}" != "${cpu}" ]
@@ -75,7 +97,11 @@ runkernel()
 	return 0
     fi
 
-    echo -n "Building ${build} ... "
+    echo -n "Building ${pbuild} ... "
+
+    if ! checkskip "${build}" ; then
+	return 0
+    fi
 
     if ! dosetup -c "${config}" -f "${fixup}" "${rootfs}" "${defconfig}"; then
 	return 1
@@ -83,21 +109,21 @@ runkernel()
 
     echo -n "running ..."
 
-    if [[ "${rootfs}" == *cpio ]]; then
-	initcli="rdinit=/sbin/init"
-	diskcmd="-initrd ${rootfs}"
-    else
-	initcli="root=/dev/sda rw"
-	diskcmd="-drive file=${rootfs},if=ide,format=raw"
+    if ! common_diskcmd "${fixup##*:}" "${rootfs}"; then
+	return 1
     fi
+
+    [[ ${dodebug} -ne 0 ]] && set -x
 
     ${QEMU} -kernel arch/x86/boot/bzImage \
 	-M ${mach} -cpu ${cpu} -no-reboot -m 256 \
 	${diskcmd} \
 	--append "earlycon=uart8250,io,0x3f8,9600n8 ${initcli} mem=256M vga=0 uvesafb.mode_option=640x480-32 oprofile.timer=1 console=ttyS0 console=tty doreboot" \
 	-nographic > ${logfile} 2>&1 &
-
     pid=$!
+
+    [[ ${dodebug} -ne 0 ]] && set +x
+
     dowait ${pid} ${logfile} manual waitlist[@]
     retcode=$?
     rm -f ${logfile}
@@ -107,29 +133,45 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
-runkernel defconfig smp Broadwell q35 rootfs.ext2
+retcode=0
+
+runkernel defconfig smp:ata Broadwell q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig smp Skylake-Client q35 rootfs.ext2
+runkernel defconfig smp:nvme IvyBridge q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig smp SandyBridge q35 rootfs.ext2
+runkernel defconfig smp:usb SandyBridge q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig smp Haswell pc rootfs.ext2
+runkernel defconfig smp:usb-uas Haswell q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig smp Nehalem q35 rootfs.ext2
+runkernel defconfig smp:mmc Skylake-Client q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig smp phenom pc rootfs.ext2
+runkernel defconfig smp:scsi[DC395] Conroe q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig smp Opteron_G5 q35 rootfs.cpio
+runkernel defconfig smp:scsi[AM53C974] Nehalem q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig smp Westmere q35 rootfs.cpio
+runkernel defconfig smp:scsi[53C810] Westmere-IBRS q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig nosmp core2duo q35 rootfs.ext2
+runkernel defconfig smp:scsi[53C895A] Skylake-Server q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig nosmp Conroe pc rootfs.ext2
+runkernel defconfig smp:scsi[MEGASAS] EPYC pc rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig nosmp Opteron_G1 pc rootfs.cpio
+runkernel defconfig smp:scsi[MEGASAS2] EPYC-IBPB q35 rootfs.ext2
 retcode=$((${retcode} + $?))
-runkernel defconfig nosmp n270 q35 rootfs.ext2
+runkernel defconfig smp:scsi[FUSION] Opteron_G5 q35 rootfs.ext2
+retcode=$((${retcode} + $?))
+runkernel defconfig smp phenom pc rootfs.cpio
+retcode=$((${retcode} + $?))
+runkernel defconfig smp Opteron_G1 q35 rootfs.cpio
+retcode=$((${retcode} + $?))
+runkernel defconfig smp:ata Opteron_G2 pc rootfs.ext2
+retcode=$((${retcode} + $?))
+runkernel defconfig smp:usb core2duo q35 rootfs.ext2
+retcode=$((${retcode} + $?))
+runkernel defconfig nosmp:usb Opteron_G3 pc rootfs.ext2
+retcode=$((${retcode} + $?))
+runkernel defconfig nosmp:ata Opteron_G4 q35 rootfs.ext2
+retcode=$((${retcode} + $?))
+runkernel defconfig nosmp:ata n270 q35 rootfs.ext2
 retcode=$((${retcode} + $?))
 
 exit ${retcode}
