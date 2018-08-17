8f233f93eeb85e8b888234919aa1cfaddac391e6
Guenter Roeck <linux@roeck-us.net>
2018-08-17 06:24:19 -0700

    mipsel: Improve infrastructure use
    Use infrastructure to handle fixups and to clean up the temp file.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/mipsel/run-qemu-mipsel.sh
+++ rootfs/mipsel/run-qemu-mipsel.sh
@@ -33,36 +33,6 @@ patch_defconfig()
     local fixups=${2//:/ }
     local fixup
 
-    # Enable DEVTMPFS and BLK_DEV_INITRD for initrd support
-    # DEVTMPFS needs to be explicitly enabled for v3.14 and older kernels.
-    echo "CONFIG_DEVTMPFS=y" >> ${defconfig}
-    echo "CONFIG_DEVTMPFS_MOUNT=y" >> ${defconfig}
-    echo "CONFIG_BLK_DEV_INITRD=y" >> ${defconfig}
-
-    # MMC/SDHCI
-    echo "CONFIG_MMC=y" >> ${defconfig}
-    echo "CONFIG_MMC_SDHCI=y" >> ${defconfig}
-    echo "CONFIG_MMC_SDHCI_PCI=y" >> ${defconfig}
-
-    # SCSI
-    echo "CONFIG_SCSI=y" >> ${defconfig}
-    echo "CONFIG_BLK_DEV_SD=y" >> ${defconfig}
-    echo "CONFIG_SCSI_DC395x=y" >> ${defconfig}
-    echo "CONFIG_SCSI_AM53C974=y" >> ${defconfig}
-    echo "CONFIG_MEGARAID_SAS=y" >> ${defconfig}
-    echo "CONFIG_FUSION=y" >> ${defconfig}
-    echo "CONFIG_FUSION_SAS=y" >> ${defconfig}
-    echo "CONFIG_SCSI_SYM53C8XX_2=y" >> ${defconfig}
-
-    # NVME
-    echo "CONFIG_BLK_DEV_NVME=y" >> ${defconfig}
-
-    # USB
-    echo "CONFIG_USB=y" >> ${defconfig}
-    echo "CONFIG_USB_XHCI_HCD=y" >> ${defconfig}
-    echo "CONFIG_USB_STORAGE=y" >> ${defconfig}
-    echo "CONFIG_USB_UAS=y" >> ${defconfig}
-
     for fixup in ${fixups}; do
 	if [[ "${fixup}" == "smp" ]]; then
 	    echo "CONFIG_MIPS_MT_SMP=y" >> ${defconfig}
@@ -80,15 +50,16 @@ runkernel()
     local rootfs=$4
     local pid
     local retcode
-    local logfile=/tmp/runkernel-$$.log
+    local logfile="$(mktemp)"
     local waitlist=("Boot successful" "Rebooting")
-    local build="mipsel:${cpu}:${defconfig}:${fixup%:*}"
-    local buildconfig="${defconfig}:${fixup%:*}"
+    local build="mipsel:${cpu}:${defconfig}:${fixup}"
+    local buildconfig="${defconfig}:${fixup//smp*/smp}"
 
-    if [[ "${rootfs}" == *cpio* ]]; then
+    addtmpfile "${logfile}"
+
+    if [[ "${rootfs%.gz}" == *cpio ]]; then
 	build+=":initrd"
     else
-	build+=":${fixup##*:}"
 	build+=":rootfs"
     fi
 
@@ -116,23 +87,17 @@ runkernel()
 	return 0
     fi
 
-    if ! dosetup -c "${buildconfig}" -f "${fixup}" "${rootfs}" "${defconfig}"; then
+    if ! dosetup -c "${buildconfig}" -F "${fixup}" "${rootfs}" "${defconfig}"; then
 	return 1
     fi
-
-    rootfs="${rootfs%.gz}"
 
     echo -n "running ..."
-
-    if ! common_diskcmd "${fixup##*:}" "${rootfs}"; then
-	return 1
-    fi
 
     [[ ${dodebug} -ne 0 ]] && set -x
 
     ${QEMU} -kernel ${KERNEL_IMAGE} -M ${QEMU_MACH} -cpu ${cpu} \
 	-vga cirrus -no-reboot -m 128 \
-	${diskcmd} \
+	${extra_params} \
 	--append "${initcli} mem=128M console=ttyS0 ${extracli}" \
 	-nographic > ${logfile} 2>&1 &
     pid=$!
@@ -141,14 +106,13 @@ runkernel()
 
     dowait ${pid} ${logfile} automatic waitlist[@]
     retcode=$?
-    rm -f ${logfile}
     return ${retcode}
 }
 
 echo "Build reference: $(git describe)"
 echo
 
-runkernel 24Kf malta_defconfig smp:ata rootfs.cpio.gz
+runkernel 24Kf malta_defconfig smp rootfs.cpio.gz
 retcode=$?
 runkernel 24Kf malta_defconfig smp:ata rootfs-mipselr1.ext2.gz
 retcode=$((retcode + $?))
@@ -184,7 +148,7 @@ retcode=$((retcode + $?))
 runkernel mips32r6-generic malta_qemu_32r6_defconfig smp:ata rootfs-mipselr6.ext2.gz
 retcode=$((retcode + $?))
 
-runkernel 24Kf malta_defconfig nosmp:ata rootfs.cpio.gz
+runkernel 24Kf malta_defconfig nosmp rootfs.cpio.gz
 retcode=$((retcode + $?))
 runkernel 24Kf malta_defconfig nosmp:ata rootfs-mipselr1.ext2.gz
 retcode=$((retcode + $?))
