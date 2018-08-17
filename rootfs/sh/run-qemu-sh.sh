134ef245071751ec0fde1a57a050f54f6272dbc5
Guenter Roeck <linux@roeck-us.net>
2018-08-17 06:26:33 -0700

    rootfs: sh: Use infra to clean up temp file
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/sh/run-qemu-sh.sh
+++ rootfs/sh/run-qemu-sh.sh
@@ -61,10 +61,11 @@ runkernel()
     local fixup=$2
     local rootfs=$3
     local pid
-    local retcode
     local logfile=$(mktemp)
     local waitlist=("Power down" "Boot successful" "Poweroff")
     local build="${ARCH}:${defconfig}"
+
+    addtmpfile "${logfile}"
 
     if [[ "${rootfs%.gz}" == *cpio ]]; then
 	build+=":initrd"
@@ -99,9 +100,7 @@ runkernel()
     [[ ${dodebug} -ne 0 ]] && set +x
 
     dowait ${pid} ${logfile} automatic waitlist[@]
-    retcode=$?
-    rm -f ${logfile}
-    return ${retcode}
+    return $?
 }
 
 echo "Build reference: $(git describe)"
