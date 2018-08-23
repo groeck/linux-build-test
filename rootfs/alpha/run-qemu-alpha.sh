9fdf0afc48f5c0e16a36c7728abbfe488eb5afe3
Guenter Roeck <linux@roeck-us.net>
2018-08-23 10:45:30 -0700

    rootfs: alpha: Use mktemp to generate temp file name
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/alpha/run-qemu-alpha.sh
+++ rootfs/alpha/run-qemu-alpha.sh
@@ -34,10 +34,11 @@ runkernel()
     local fixup=$2
     local rootfs=$3
     local pid
-    local retcode
-    local logfile=/tmp/runkernel-$$.log
+    local logfile="$(mktemp)"
     local waitlist=("Boot successful" "Rebooting" "Restarting system")
     local build="${ARCH}:${defconfig}"
+
+    addtmpfile "${logfile}"
 
     if [[ "${rootfs}" == *cpio ]]; then
 	build+=":initrd"
@@ -72,9 +73,7 @@ runkernel()
     [[ ${dodebug} -ne 0 ]] && set +x
 
     dowait ${pid} ${logfile} auto waitlist[@]
-    retcode=$?
-    rm -f ${logfile}
-    return ${retcode}
+    return $?
 }
 
 echo "Build reference: $(git describe)"
