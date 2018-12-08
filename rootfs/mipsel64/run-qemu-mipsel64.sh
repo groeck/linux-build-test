37cffea102e3a20f9f3a2a30d1de06d09950acec
Guenter Roeck <linux@roeck-us.net>
2018-12-07 16:39:06 -0800

    rootfs: mipsel64: Switch to kernel.org toolchain
    Replace yocto/poky compiler with kernel.org compiler.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/mipsel64/run-qemu-mipsel64.sh
+++ rootfs/mipsel64/run-qemu-mipsel64.sh
@@ -15,13 +15,10 @@ rel=$(git describe | cut -f1 -d- | cut -f1,2 -d.)
 case "${rel}" in
 v3.16)
 	# v3.16 needs the old version of gcc
-	PATH_MIPS=/opt/poky/1.3/sysroots/x86_64-pokysdk-linux/usr/bin/mips32-poky-linux
-	PREFIX=mips-poky-linux-
+	PATH_MIPS=/opt/kernel/gcc-4.9.4-nolibc/mips64-linux/bin
+	PREFIX=mips64-linux-
 	;;
 *)
-	# Using Poky 2.0 appears to result in random crashes due to stack
-	# corruption (?). No idea if this is a qemu or a compiler problem.
-	# PATH_MIPS=/opt/poky/2.0/sysroots/x86_64-pokysdk-linux/usr/bin/mips-poky-linux
 	PATH_MIPS=/opt/kernel/gcc-7.3.0-nolibc/mips64-linux/bin
 	PREFIX=mips64-linux-
 	;;
