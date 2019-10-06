772e1d1c0566c60d471f4d7019af1bf0342e7203
Guenter Roeck <linux@roeck-us.net>
2019-10-06 12:52:17 -0700

    rootfs: microblaze: Change reboot wait string
    Stating with v5.4, "Machine restart" no longer works.
    Use "Restarting system" instead.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/microblaze/run-qemu-microblaze.sh
+++ rootfs/microblaze/run-qemu-microblaze.sh
@@ -20,7 +20,7 @@ runkernel()
     local mach=$2
     local console=$3
     local pid
-    local waitlist=("Machine restart" "Boot successful" "Rebooting")
+    local waitlist=("Restarting system" "Boot successful" "Rebooting")
     local logfile="$(__mktemp)"
 
     echo -n "Building ${ARCH}:${defconfig} ... "
