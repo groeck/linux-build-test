2ab240aabca197c2e98c1af29822674c00136905
Guenter Roeck <linux@roeck-us.net>
2019-11-09 07:55:39 -0800

    rootfs: m68k: Switch to qemu v4.2
    qemu v4.2 supports q800, so we can now use one version of qemu
    for all tests.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/m68k/run-qemu-m68k.sh
+++ rootfs/m68k/run-qemu-m68k.sh
@@ -8,7 +8,7 @@ shift $((OPTIND - 1))
 
 machine=$1
 
-QEMU=${QEMU:-${QEMU_BIN}/qemu-system-m68k}
+QEMU=${QEMU:-${QEMU_V42_BIN}/qemu-system-m68k}
 PREFIX=m68k-linux-
 ARCH=m68k
 PATH_M68K=/opt/kernel/gcc-7.3.0-nolibc/m68k-linux/bin
@@ -89,13 +89,6 @@ runkernel()
     fi
 
     echo -n "running ..."
-
-    if [[ "${mach}" = "q800" ]]; then
-	# q800 needs special qemu, which in turn does not support mcf5208evb
-	# Note: Starting with qemu v4.2, q800 support will be available
-	# in the upstream version of qemu.
-	qemu="${QEMU_V40_M68K_BIN}/qemu-system-m68k"
-    fi
 
     [[ ${dodebug} -ne 0 ]] && set -x
 
