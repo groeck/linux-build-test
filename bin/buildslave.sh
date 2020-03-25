11b5b47f2a85770ca9590e1276f33d715fff026d
Guenter Roeck <linux@roeck-us.net>
2020-03-25 16:14:30 -0700

    bin/buildslave.sh: Remove stale pid file
    After reboot the old pid file is typically still around.
    The start script then tries to kill the associated process, which
    likely fails (or creates havoc), and the slave doesn't start.
    Remove the file if it is stale to prevent that from happening.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/buildslave.sh
+++ bin/buildslave.sh
@@ -2,7 +2,18 @@
 
 rootdir="/opt/buildbot"
 vdir="${rootdir}/virtualenv"
-pidfile="${buildbotdir}/twistd.pid"
+pidfile="${rootdir}/slave/twistd.pid"
+
+if [[ -e "${pidfile}" ]]; then
+    pid="$(cat ${pidfile})"
+    cmd=$(ps -o ucmd= ${pid} | awk '{ print $1 }')
+    if [[ "${cmd}" != "buildslave" ]]; then
+	# The pid file exists, and a process may be associated with it,
+	# but it does not point to a buildslave.
+	# This may prevent the slave from starting. Remove the file.
+	rm -f "${pidfile}"
+    fi
+fi
 
 if [[ ! -d "${vdir}" ]]; then
     if [[ ! -x /usr/bin/virtualenv ]]; then
