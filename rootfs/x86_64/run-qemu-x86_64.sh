6c4bd0a738b0a8ae2354b1829e141b7a251a7cd2
Guenter Roeck <linux@roeck-us.net>
2021-03-13 18:36:38 -0800

    rootfs: x86_64: Add networking tests
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/x86_64/run-qemu-x86_64.sh
+++ rootfs/x86_64/run-qemu-x86_64.sh
@@ -18,7 +18,7 @@ PREFIX="x86_64-linux-"
 
 PATH=${PATH_X86}:${PATH}
 
-skip_44="defconfig:smp4:efi32:mem256:scsi[DC395]"
+skip_44="defconfig:smp4:net,tulip:efi32:mem256:scsi[DC395]"
 
 patch_defconfig()
 {
@@ -91,87 +91,87 @@ retcode=0
 
 # runkernel defconfig kvm64 q35
 # retcode=$((retcode + $?))
-runkernel defconfig smp:mem256:ata Broadwell-noTSX q35 rootfs.ext2
+runkernel defconfig smp:net,e1000:mem256:ata Broadwell-noTSX q35 rootfs.ext2
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig smp:mem256:ata Cascadelake-Server q35 rootfs.iso
+runkernel defconfig smp:net,e1000e:mem256:ata Cascadelake-Server q35 rootfs.iso
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig smp2:efi:mem512:nvme IvyBridge q35 rootfs.btrfs
+runkernel defconfig smp2:net,i82801:efi:mem512:nvme IvyBridge q35 rootfs.btrfs
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig smp4:efi32:mem1G:usb SandyBridge q35 rootfs.squashfs
+runkernel defconfig smp4:net,ne2k_pci:efi32:mem1G:usb SandyBridge q35 rootfs.squashfs
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig smp:mem2G:usb-uas Haswell q35 rootfs.ext2
+runkernel defconfig smp:net,pcnet:mem2G:usb-uas Haswell q35 rootfs.ext2
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig smp2:efi:mem4G:sdhci:mmc Skylake-Client q35 rootfs.ext2
-retcode=$((retcode + $?))
-checkstate ${retcode}
-
-runkernel defconfig smp4:efi32:mem256:scsi[DC395] Conroe q35 rootfs.ext2
-retcode=$((retcode + $?))
-checkstate ${retcode}
-runkernel defconfig smp:mem512:scsi[AM53C974] Nehalem q35 rootfs.ext2
+runkernel defconfig smp2:net,rtl8139:efi:mem4G:sdhci:mmc Skylake-Client q35 rootfs.ext2
 retcode=$((retcode + $?))
 checkstate ${retcode}
 
-runkernel defconfig smp2:efi:mem1G:scsi[53C810] Westmere-IBRS q35 rootfs.iso
+runkernel defconfig smp4:net,tulip:efi32:mem256:scsi[DC395] Conroe q35 rootfs.ext2
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig smp4:efi32:mem2G:scsi[53C895A] Skylake-Server q35 rootfs.ext2
+runkernel defconfig smp:net,virtio-net:mem512:scsi[AM53C974] Nehalem q35 rootfs.ext2
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig smp:mem4G:scsi[FUSION] EPYC pc rootfs.btrfs
+
+runkernel defconfig smp2:net,usb-net:efi:mem1G:scsi[53C810] Westmere-IBRS q35 rootfs.iso
+retcode=$((retcode + $?))
+checkstate ${retcode}
+runkernel defconfig smp4:net,e1000:efi32:mem2G:scsi[53C895A] Skylake-Server q35 rootfs.ext2
+retcode=$((retcode + $?))
+checkstate ${retcode}
+runkernel defconfig smp:net,e1000-82544gc:mem4G:scsi[FUSION] EPYC pc rootfs.btrfs
 retcode=$((retcode + $?))
 checkstate ${retcode}
 # efi combined with scsi[FUSION] fails
-runkernel defconfig smp2:efi:mem8G:scsi[MEGASAS] EPYC-IBPB q35 rootfs.ext2
+runkernel defconfig smp2:net,e1000-82545em:efi:mem8G:scsi[MEGASAS] EPYC-IBPB q35 rootfs.ext2
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig smp4:efi32:mem256:scsi[MEGASAS2] Opteron_G5 q35 rootfs.ext2
+runkernel defconfig smp4:net,i82559c:efi32:mem256:scsi[MEGASAS2] Opteron_G5 q35 rootfs.ext2
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig smp:mem512 phenom pc rootfs.cpio
+runkernel defconfig smp:net,i82559er:mem512 phenom pc rootfs.cpio
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig smp2:efi:mem1G Opteron_G1 q35 rootfs.cpio
+runkernel defconfig smp2:net,i82562:efi:mem1G Opteron_G1 q35 rootfs.cpio
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig smp:efi32:mem2G:scsi[virtio-pci] Opteron_G2 pc rootfs.ext2
+runkernel defconfig smp:net,i82559b:efi32:mem2G:scsi[virtio-pci] Opteron_G2 pc rootfs.ext2
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig smp2:mem4G:virtio-pci core2duo q35 rootfs.ext2
+runkernel defconfig smp2:net,i82559a:mem4G:virtio-pci core2duo q35 rootfs.ext2
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig smp4:efi:mem8G:virtio Broadwell q35 rootfs.ext2
+runkernel defconfig smp4:net,i82558b:efi:mem8G:virtio Broadwell q35 rootfs.ext2
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig smp2:efi32:mem1G:virtio Nehalem q35 rootfs.ext2
-retcode=$((retcode + $?))
-checkstate ${retcode}
-
-runkernel defconfig preempt:smp4:efi:mem2G:virtio Icelake-Client q35 rootfs.iso
-retcode=$((retcode + $?))
-checkstate ${retcode}
-runkernel defconfig preempt:smp8:mem4G:nvme Icelake-Server q35 rootfs.btrfs
-retcode=$((retcode + $?))
-checkstate ${retcode}
-runkernel defconfig preempt:smp2:efi32:mem1G:sdhci:mmc Skylake-Client-IBRS q35 rootfs.ext2
-retcode=$((retcode + $?))
-checkstate ${retcode}
-runkernel defconfig preempt:smp6:mem512 KnightsMill q35 rootfs.cpio
+runkernel defconfig smp2:net,i82558a:efi32:mem1G:virtio Nehalem q35 rootfs.ext2
 retcode=$((retcode + $?))
 checkstate ${retcode}
 
-runkernel defconfig nosmp:mem1G:usb Opteron_G3 pc rootfs.ext2
+runkernel defconfig preempt:smp4:net,ne2k_pci:efi:mem2G:virtio Icelake-Client q35 rootfs.iso
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig nosmp:efi:mem512:ata Opteron_G4 q35 rootfs.ext2
+runkernel defconfig preempt:smp8:net,i82557a:mem4G:nvme Icelake-Server q35 rootfs.btrfs
 retcode=$((retcode + $?))
 checkstate ${retcode}
-runkernel defconfig nosmp:efi32:mem2G:ata Haswell-noTSX-IBRS q35 rootfs.ext2
+runkernel defconfig preempt:smp2:net,i82558b:efi32:mem1G:sdhci:mmc Skylake-Client-IBRS q35 rootfs.ext2
+retcode=$((retcode + $?))
+checkstate ${retcode}
+runkernel defconfig preempt:smp6:net,i82550:mem512 KnightsMill q35 rootfs.cpio
+retcode=$((retcode + $?))
+checkstate ${retcode}
+
+runkernel defconfig nosmp:net,e1000:mem1G:usb Opteron_G3 pc rootfs.ext2
+retcode=$((retcode + $?))
+checkstate ${retcode}
+runkernel defconfig nosmp:net,ne2k_pci:efi:mem512:ata Opteron_G4 q35 rootfs.ext2
+retcode=$((retcode + $?))
+checkstate ${retcode}
+runkernel defconfig nosmp:net,pcnet:efi32:mem2G:ata Haswell-noTSX-IBRS q35 rootfs.ext2
 retcode=$((retcode + $?))
 
 exit ${retcode}
