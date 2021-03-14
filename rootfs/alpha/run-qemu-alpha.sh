11d189d5545aa2d496d3861cd0e251b0fa3e7e66
Guenter Roeck <linux@roeck-us.net>
2021-03-14 11:38:00 -0700

    rootfsa: alpha: use gcc 10.2.0
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/alpha/run-qemu-alpha.sh
+++ rootfs/alpha/run-qemu-alpha.sh
@@ -15,7 +15,7 @@ PREFIX=alpha-linux-
 ARCH=alpha
 
 rel=$(git describe | cut -f1 -d- | cut -f1,2 -d.)
-PATH_ALPHA=/opt/kernel/gcc-6.4.0-nolibc/alpha-linux/bin
+PATH_ALPHA=/opt/kernel/gcc-10.2.0-nolibc/alpha-linux/bin
 
 PATH=${PATH_ALPHA}:${PATH}
 
