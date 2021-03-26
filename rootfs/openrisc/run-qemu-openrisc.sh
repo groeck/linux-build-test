a3b8a3c1a0208d98f357134d80215500f6498eb7
Guenter Roeck <linux@roeck-us.net>
2021-03-25 21:40:54 -0700

    rootfs: openrisc: New root file system
    Also remove obsolete support for pre-4.4 kernels (elf32-or32),
    and add support for debug options.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/openrisc/run-qemu-openrisc.sh
+++ rootfs/openrisc/run-qemu-openrisc.sh
@@ -4,10 +4,13 @@ dir=$(cd $(dirname $0); pwd)
 . ${dir}/../scripts/config.sh
 . ${dir}/../scripts/common.sh
 
+parse_args "$@"
+shift $((OPTIND - 1))
+
 QEMU=${QEMU:-${QEMU_BIN}/qemu-system-or1k}
 PREFIX=or1k-linux-
 ARCH=openrisc
-rootfs=busybox-openrisc.cpio
+rootfs=rootfs.cpio
 PATH_OR32=/opt/kernel/gcc-9.3.0-nolibc/or1k-linux/bin
 
 PATH=${PATH_OR32}:${PATH}
@@ -28,29 +31,12 @@ patch_defconfig()
 runkernel()
 {
     local defconfig=$1
-    local retcode
     local waitlist=("Restarting system" "Boot successful" "Rebooting")
-    local fixup=0
 
     echo -n "Building ${ARCH}:${defconfig} ... "
 
-    # Kernel may expect elf32-or32, but toolchain produces elf32-or1k.
-    # Kludgy fix until we find a better solution.
-    grep "elf32-or1k" arch/openrisc/kernel/vmlinux.lds.S >/dev/null 2>&1
-    fixup=$?
-    if [ ${fixup} -ne 0 ]
-    then
-        sed -i -e 's/elf32-or32/elf32-or1k/g' arch/openrisc/kernel/vmlinux.lds.S
-    fi
-    dosetup -f fixup "${rootfs}" "${defconfig}"
-    retcode=$?
-    if [ ${fixup} -ne 0 ]
-    then
-        sed -i -e 's/elf32-or1k/elf32-or32/g' arch/openrisc/kernel/vmlinux.lds.S
-    fi
-    if [ ${retcode} -ne 0 ]
-    then
-	return ${retcode}
+    if ! dosetup -F "net,default" "${rootfs}" "${defconfig}"; then
+	return 1
     fi
 
     execute manual waitlist[@] \
