9dbc23043a7adf93f257b46f1187323b7b8b05d7
Guenter Roeck <linux@roeck-us.net>
2021-03-25 22:33:05 -0700

    rootfs: microblaze: New root file system with network tests
    Remove 'dynamic' run.sh creation; it is just confusing and
    not really that valuable. Run network test where applicable.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/microblaze/run-qemu-microblaze.sh
+++ rootfs/microblaze/run-qemu-microblaze.sh
@@ -13,15 +13,21 @@ PREFIX=microblaze-linux-
 ARCH=microblaze
 rootfs=rootfs.cpio
 
-PATH_MICROBLAZE=/opt/kernel/gcc-9.3.0-nolibc/microblaze-linux/bin
+PATH_MICROBLAZE="/opt/kernel/gcc-9.3.0-nolibc/microblaze-linux/bin"
 
-PATH=${PATH_MICROBLAZE}:${PATH}
+PATH="${PATH_MICROBLAZE}:${PATH}"
+
+patch_defconfig()
+{
+    :
+}
 
 runkernel()
 {
     local defconfig=$1
     local mach=$2
-    local console=$3
+    local fixup=$3
+    local console=$4
     local waitlist=("Restarting system" "Boot successful" "Rebooting")
 
     if ! match_params "${machine}@${mach}"; then
@@ -31,12 +37,11 @@ runkernel()
 
     echo -n "Building ${ARCH}:${defconfig} ... "
 
-    dosetup -d "${rootfs}" "${defconfig}"
-    if [ $? -ne 0 ]; then
+    if ! dosetup -F "${fixup}" "${rootfs}" "${defconfig}"; then
 	return 1
     fi
 
-    initcli+=" rdinit=/sbin/init console=${console},115200"
+    initcli+=" console=${console},115200"
 
     execute manual waitlist[@] \
       ${QEMU} -M ${mach} -m 256 \
@@ -51,10 +56,12 @@ runkernel()
 echo "Build reference: $(git describe)"
 echo
 
+# locking tests result in hard lockup
+
 retcode=0
-runkernel qemu_microblaze_defconfig petalogix-s3adsp1800 ttyUL0
+runkernel qemu_microblaze_defconfig petalogix-s3adsp1800 "nolocktests:net,default" ttyUL0
 retcode=$((retcode + $?))
-runkernel qemu_microblaze_ml605_defconfig petalogix-ml605 ttyS0
+runkernel qemu_microblaze_ml605_defconfig petalogix-ml605 "nolocktests" ttyS0
 retcode=$((retcode + $?))
 
 exit ${retcode}
