477111f086e4fc433beece16429627d2725508ad
Guenter Roeck <linux@roeck-us.net>
2022-09-20 22:33:43 -0700

    rootfs: scripts: Disable CONFIG_WW_MUTEX_SELFTEST entirely
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/scripts/common.sh
+++ rootfs/scripts/common.sh
@@ -1013,10 +1013,8 @@ __setup_fragment()
 	    enable_config "${fragment}" CONFIG_PROVE_RCU CONFIG_PROVE_LOCKING CONFIG
 	    # takes too long
 	    # enable_config "${fragment}" CONFIG_TORTURE_TEST CONFIG_LOCK_TORTURE_TEST CONFIG_RCU_TORTURE_TEST
-	    if ! grep -q "CONFIG_PREEMPT=y" "${defconfig}"; then
-	        # CONFIG_WW_MUTEX_SELFTEST interferes with CONFIG_PREEMPT=y
-	        enable_config "${fragment}" CONFIG_WW_MUTEX_SELFTEST
-	    fi
+	    # CONFIG_WW_MUTEX_SELFTEST interferes with CONFIG_PREEMPT=y
+	    # enable_config "${fragment}" CONFIG_WW_MUTEX_SELFTEST
 	fi
 
 	enable_config "${fragment}" CONFIG_RBTREE_TEST CONFIG_INTERVAL_TREE_TEST
