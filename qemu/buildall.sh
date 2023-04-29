6522f6a11a218718a81f00e4a33f681572895df2
Guenter Roeck <linux@roeck-us.net>
2023-04-29 08:45:23 -0700

    qemu: buildall: Drop obsolete qemu versions
    qemu v6.2, v7.0, and v7.1 are no longer used. Drop support for
    building those versions.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- qemu/buildall.sh
+++ qemu/buildall.sh
@@ -114,27 +114,6 @@ if [ -z "$1" -o "$1" = "v4.2" ]; then
     fi
 fi
 
-if [ -z "$1" -o "$1" = "v6.2" ]; then
-    dobuild_common v6.2.0-local v6.2 "--disable-vnc-png"
-    if [ -n "$2" ]; then
-	shift
-    fi
-fi
-
-if [ -z "$1" -o "$1" = "v7.0" ]; then
-    dobuild_common v7.0.0-local v7.0 "--disable-vnc-png"
-    if [ -n "$2" ]; then
-	shift
-    fi
-fi
-
-if [ -z "$1" -o "$1" = "v7.1" ]; then
-    dobuild_common v7.1.0-local v7.1 "--disable-png"
-    if [ -n "$2" ]; then
-	shift
-    fi
-fi
-
 if [ -z "$1" -o "$1" = "v7.2" ]; then
     dobuild_common v7.2.2-local v7.2 "--disable-png"
     if [ -n "$2" ]; then
