9a3cc0803e4e089ba5b6bacb80a509cade1bbf83
Guenter Roeck <linux@roeck-us.net>
2023-06-22 18:06:01 -0700

    rootfs: arm-aspeed: Handle dtb file move
    In v6.5, dtb files in arch/arm will be moved. Handle old and new
    location.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/arm-aspeed/run-qemu-arm-aspeed.sh
+++ rootfs/arm-aspeed/run-qemu-arm-aspeed.sh
@@ -77,7 +77,7 @@ runkernel()
     local fixup=$6
     local dtb=$7
     local ddtb="${dtb%.dtb}"
-    local dtbfile="arch/arm/boot/dts/${dtb}"
+    local dtbfile=""
     local nonet=0
     local logfile="$(__mktemp)"
     local waitlist=("Restarting" "Boot successful" "Rebooting")
@@ -133,8 +133,9 @@ runkernel()
     # If a dtb file was specified but does not exist, skip the build.
     local dtbcmd=""
     if [[ -n "${dtb}" ]]; then
-	if [[ ! -e "${dtbfile}" ]]; then
-	    echo "skipped"
+	dtbfile="$(find arch/arm/boot/dts -name "${dtb}")"
+	if [[ -z "${dtbfile}" ]]; then
+	    echo "skipped (dtb)"
 	    return 0
 	fi
 	dtbcmd="-dtb ${dtbfile}"
