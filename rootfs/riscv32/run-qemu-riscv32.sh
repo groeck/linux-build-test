902e3ac613ee9e5417bc07dc51a05d38d786b760
Guenter Roeck <linux@roeck-us.net>
2023-07-22 23:47:18 -0700

    rootfs: riscv32: Test tpm-tis-device
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/riscv32/run-qemu-riscv32.sh
+++ rootfs/riscv32/run-qemu-riscv32.sh
@@ -26,6 +26,9 @@ patch_defconfig()
     echo "CONFIG_PCI_HOST_GENERIC=y" >> ${defconfig}
     echo "CONFIG_CGROUP_FREEZER=n" >> ${defconfig}
 
+    # Needed for TPM tests
+    enable_config "${defconfig}" CONFIG_TCG_TPM CONFIG_TCG_TIS
+
     # CONFIG_PREEMPT=y and some of the selftests are like cat and dog,
     # only worse.
     if grep -q "CONFIG_PREEMPT=y" "${defconfig}"; then
@@ -104,7 +107,7 @@ runkernel virt "" rv32_defconfig net,i82801:virtio rootfs.ext2
 retcode=$((retcode + $?))
 runkernel virt "rv32,zbb=no" rv32_defconfig net,i82550:virtio-pci rootfs.ext2
 retcode=$((retcode + $?))
-runkernel virt "" rv32_defconfig net,e1000-82544gc:sdhci:mmc rootfs.ext2
+runkernel virt "" rv32_defconfig tpm-tis-device:net,e1000-82544gc:sdhci:mmc rootfs.ext2
 retcode=$((retcode + $?))
 runkernel virt "rv32,zbb=no" rv32_defconfig net,usb-ohci:nvme rootfs.ext2
 retcode=$((retcode + $?))
