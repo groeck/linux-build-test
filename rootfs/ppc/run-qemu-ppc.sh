36bab2d78f97e28aaf735f4744056f394057e18f
Guenter Roeck <linux@roeck-us.net>
2024-02-20 11:49:19 -0800

    rootfs: ppc: Enable bcachefs
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/ppc/run-qemu-ppc.sh
+++ rootfs/ppc/run-qemu-ppc.sh
@@ -53,6 +53,7 @@ patch_defconfig()
 	    enable_config ${defconfig} CONFIG_JFS_FS
 	    enable_config ${defconfig} CONFIG_NILFS2_FS
 	    enable_config ${defconfig} CONFIG_XFS_FS
+	    enable_config ${defconfig} CONFIG_BCACHEFS_FS
 	    # enable for testing
 	    enable_config ${defconfig} CONFIG_F2FS_FS
 	    enable_config ${defconfig} CONFIG_MINIX_FS
