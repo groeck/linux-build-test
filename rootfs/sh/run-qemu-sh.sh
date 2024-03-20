74f78c6ecf984fb81ec1aa120f284102499d074b
Guenter Roeck <linux@roeck-us.net>
2024-03-20 12:34:42 -0700

    rootfs: sh: Drop workarounds for v5.4.y and older kernel branches
    We no longer test v5.4.y and older for sh4, so drop related workarounds.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/sh/run-qemu-sh.sh
+++ rootfs/sh/run-qemu-sh.sh
@@ -20,20 +20,7 @@ EARLYCON=""
 
 errlog="/tmp/err-sh.log"
 
-if [[ ${linux_version_code} -lt $(kernel_version 5 10) ]]; then
-    # boot tests hang with gcc 9.x and later kernels older than v5.10
-    # when using recent binutils (2.37 or later).
-    # Use gcc 11.3.0 with binutils 2.32 instead.
-    PATH_SH=/opt/kernel/gcc-11.3.0-2.32-nolibc/sh4-linux/bin
-else
-    PATH_SH=/opt/kernel/${DEFAULT_CC}/sh4-linux/bin
-fi
-
-if [[ ${linux_version_code} -ge $(kernel_version 5 0) ]]; then
-    # earlycon only works with v5.0+ and otherwise results in a crash.
-    CONFIG="CONFIG_SERIAL_SH_SCI_EARLYCON=y"
-    EARLYCON="earlycon=scif,mmio16,0xffe80000"
-fi
+PATH_SH=/opt/kernel/${DEFAULT_CC}/sh4-linux/bin
 
 PATH=${PATH_SH}:${PATH}
 
