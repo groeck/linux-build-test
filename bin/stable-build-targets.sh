1512ed6278fabc5c9c944ddad45b6a533d916ed2
Guenter Roeck <linux@roeck-us.net>
2024-03-21 09:39:28 -0700

    stable-build-target: Re-enable CONFIG_DRM_SUN4I
    A patch is pending to fix the build problem on affected architectures.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/stable-build-targets.sh
+++ bin/stable-build-targets.sh
@@ -59,19 +59,15 @@ skip_515="x86_64:tools/perf i386:tools/perf"
 
 # fixups
 
-# CONFIG_DRM_SUN4I is disabled on some architectures to avoid 64-bit build
-# failures.
-# Affects:
-# - openrisc
-# - parisc
-# - xtensa
-
 # CONFIG_WERROR is disabled on some architectures where builds are known to fail
 # if it is enabled.
 # Affects:
 # - alpha
 # - sh4
 # - sparc64
+#
+# CONFIG_DRM_XE is disabled for mips due to "duplicate 'END' defines"
+# build failures.
 
 fixup_alpha=("s/CONFIG_WERROR=y/CONFIG_WERROR=n/")
 
@@ -80,13 +76,9 @@ fixup_arc=("s/CONFIG_BLK_DEV_INITRD=y/CONFIG_BLK_DEV_INITRD=n/"
 
 fixup_csky=("s/CONFIG_FRAME_POINTER=y/CONFIG_FRAME_POINTER=n/")
 
-# Avoid build failures in v6.9+ due to duplicate 'END' defines
 fixup_mips=("/CONFIG_DRM_XE/d")
 
-fixup_openrisc=("/CONFIG_DRM_SUN4I=/d")
-
 fixup_parisc=("s/# CONFIG_MLONGCALLS is not set/CONFIG_MLONGCALLS=y/"
-	"/CONFIG_DRM_SUN4I=/d"
 	"s/CONFIG_MLONGCALLS=n/CONFIG_MLONGCALLS=y/")
 
 fixup_sh=("s/CONFIG_WERROR=y/CONFIG_WERROR=n/")
@@ -94,7 +86,6 @@ fixup_sh=("s/CONFIG_WERROR=y/CONFIG_WERROR=n/")
 fixup_sparc64=("s/CONFIG_WERROR=y/CONFIG_WERROR=n/")
 
 fixup_xtensa=("s/# CONFIG_LD_NO_RELAX is not set/CONFIG_LD_NO_RELAX=y/"
-	"/CONFIG_DRM_SUN4I=/d"
 	"s/CONFIG_SECTION_MISMATCH_WARN_ONLY is not set/CONFIG_SECTION_MISMATCH_WARN_ONLY=y/"
 	"s/CONFIG_LD_NO_RELAX=n/CONFIG_LD_NO_RELAX=y/")
 
