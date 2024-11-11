96aebf51fbb7d9c273aa9317c0fabf55ab2a38f1
Guenter Roeck <linux@roeck-us.net>
2024-11-11 09:50:38 -0800

    buildbot: Add cleanup call to buildbot start script
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/buildbot.sh
+++ bin/buildbot.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+cleanup()
+{
+    for dest in $*; do
+	echo "${dest}: Cleaning up /tmp"
+	if ! ssh -t ${dest} /opt/buildbot/bin/cleanup-tmp.sh >/dev/null 2>&1; then
+	   echo "    Failed to clean up ${dest}"
+	fi
+    done
+}
+
+# Clean up /tmp/ when starting buildbot to avoid unnecessary build
+# failures due to disk space problems.
+if [[ "$1" = "start" ]]; then
+    cleanup desktop mars jupiter saturn server
+fi
+
 rootdir="/opt/buildbot"
 vdir="${rootdir}/virtualenv"
 pidfile="${buildbotdir}/twistd.pid"
