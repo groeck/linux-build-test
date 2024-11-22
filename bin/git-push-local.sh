200b65ca6c117d992a1af36718be2a5e0a01d866
Guenter Roeck <linux@roeck-us.net>
2024-11-21 21:20:12 -0800

    bin/git-push-local.sh: Various optimizations
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/git-push-local.sh
+++ bin/git-push-local.sh
@@ -3,39 +3,35 @@
 git remote | grep local >/dev/null 2>&1
 if [ $? -ne 0 ]
 then
-	git remote add local git://server.roeck-us.net/git/linux-stable.git
-	if [ $? -ne 0 ]
-	then
+	if ! git remote add local git://server.roeck-us.net/git/linux-stable.git; then
 		exit 1
 	fi
-	git remote set-url --push local ssh://git@server.roeck-us.net/var/cache/git/linux-stable.git
-	if [ $? -ne 0 ]
-	then
+	if ! git remote set-url --push local ssh://git@server.roeck-us.net/var/cache/git/linux-stable.git; then
 		exit 1
 	fi
 fi
 
 git fetch --all
 
-for a in $(git branch -r | grep "origin/linux-[4-9]" | cut -f2 -d'/')
-do
+epoch="$(date +%s)"
+mepoch="$((epoch - 2500000))"  # ~ one month back
+
+for a in $(git branch -r | grep "origin/linux-[4-9]" | cut -f2 -d'/'); do
+	lepoch="$(git log -1 --format=%ct origin/$a)"
+	if [[ -z "${lepoch}" ]] || [[ "${lepoch}" -lt "${mepoch}" ]]; then
+		continue
+	fi
 	echo -n "$a: "
-	git push local origin/$a:$a
-	if [ $? -ne 0 ]
-	then
+	if ! git push local origin/$a:$a; then
 		# Try again. Create local branch, then push it.
 		# If that doesn't work, give up.
 		echo Trying to clone new upstream branch $a
 		git branch -D $a >/dev/null 2>&1
-		git checkout -b $a origin/$a
-		if [ $? -ne 0 ]
-		then
+		if ! git checkout -b $a origin/$a; then
 			echo "git checkout error (branches): aborting"
 			exit 1
 		fi
-		git push local $a
-		if [ $? -ne 0 ]
-		then
+		if ! git push local $a; then
 			echo "git push error (branches): aborting"
 			exit 1
 		fi
@@ -44,18 +40,13 @@ do
 	fi
 done
 
-epoch=$(date +%s)
-mepoch=$((${epoch} - 2500000))  # ~ one month back
-
-git tag -l | while read a
-do
-	tepoch=$(git log -1 --format=%ct $a)
-	if [ -n "${tepoch}" ]
-	then
-		if [ ${tepoch} -gt ${mepoch} ]
-		then
-			echo -n "tag:$a: "
-			git push local refs/tags/$a
+tags="$(git tag -l)"
+for tag in ${tags}; do
+	tepoch="$(git log -1 --format=%ct ${tag})"
+	if [[ -n "${tepoch}" ]]; then
+		if [[ "${tepoch}" -gt "${mepoch}" ]]; then
+			echo -n "tag:${tag}: "
+			git push local "refs/tags/${tag}"
 		fi
 	fi
 done
