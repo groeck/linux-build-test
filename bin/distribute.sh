04f23647261439175fffdea2bf9659a9c97e6a3c
Guenter Roeck <linux@roeck-us.net>
2024-11-25 07:02:24 -0800

    ditribute: Pick up workers from config file
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/distribute.sh
+++ bin/distribute.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sys=$(uname -n | cut -f1 -d.)
-if [ "${sys}" != "server" ]
-then
+sys="$(uname -n | cut -f1 -d.)"
+if [ "${sys}" != "server" ]; then
 	echo "$0: must run on server"
 	exit 1
 fi
@@ -10,39 +9,34 @@ fi
 base=/opt/buildbot
 kbase=/opt/kernel
 
-# SYSTEMS="saturn.roeck-us.net desktop jupiter mars neptune hyperion titan"
-SYSTEMS="saturn desktop jupiter mars neptune"
+progdir="$(dirname "$0")"
+cd "${progdir}/../master" || exit 1
 
-for system in ${SYSTEMS}
-do
-	echo -n "${system}: "
-	ping -c 1 -w 1 ${system} >/dev/null
-	if [ $? -ne 0 ]
-	then
+workers="$(python3 -c "from config import workers; print(' '.join(workers))")"
+for worker in ${workers}; do
+	if [[ "${worker}" == "${sys}" ]]; then
+		continue
+	fi
+	echo -n "${worker}: "
+	if ! ping -c 1 -w 1 "${worker}" >/dev/null; then
 		echo "not responding"
 		continue
 	fi
-	rsync -l --delete --timeout=15 -r ${base}/bin ${base}/rootfs ${base}/share \
+	if ! rsync -l --delete --timeout=15 -r ${base}/bin ${base}/rootfs ${base}/share \
 		${base}/.git \
 		${base}/lib ${base}/include ${base}/kconfig ${base}/qemu-install \
-		${system}:${base}
-	if [ $? -ne 0 ]
-	then
+		${worker}:${base}; then
 		echo "failed"
 	else
 		echo "done"
 	fi
 done
 
-if [ -d /opt/buildbot/virtual/disk ]
-then
+if [ -d /opt/buildbot/virtual/disk ]; then
 	echo -n "virtual: "
-	rsync -l --delete -r ${base}/bin ${base}/rootfs ${base}/share \
+	if ! rsync -l --delete -r ${base}/bin ${base}/rootfs ${base}/share \
 		${base}/lib ${base}/include ${base}/kconfig ${base}/qemu-install \
-		/opt/buildbot/virtual/disk/${base}
-	result=$?
-	if [ ${result} -ne 0 ]
-	then
+		/opt/buildbot/virtual/disk/${base}; then
 		echo "failed"
 	else
 		echo "done"
