a085e2bb32b72fb918b6069f6bff5b4d78949939
Guenter Roeck <linux@roeck-us.net>
2024-11-25 07:03:07 -0800

    cleanup-worker[s]: Pick workers from config file
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/cleanup-workers.sh
+++ bin/cleanup-workers.sh
@@ -1,4 +1,7 @@
-workers="server jupiter mars saturn desktop neptune"
+progdir="$(dirname "$0")"
+cd "${progdir}/../master" || exit 1
+workers="$(python3 -c "from config import workers; print(' '.join(workers))")"
+
 cmd="/opt/buildbot/bin/cleanup-worker.sh"
 
 for worker in ${workers}
