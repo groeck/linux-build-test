2bf8aafe41db96764640af628e7b1b09f7a67a54
Guenter Roeck <linux@roeck-us.net>
2024-11-25 07:04:04 -0800

    stop/restart-workers: Pick up workers from config file
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/restart-workers.sh
+++ bin/restart-workers.sh
@@ -1,4 +1,6 @@
-workers="server jupiter mars saturn desktop"
+progdir="$(dirname "$0")"
+cd "${progdir}/../master" || exit 1
+workers="$(python3 -c "from config import workers; print(' '.join(workers))")"
 cmd="/opt/buildbot/bin/start-worker.sh"
 
 for worker in ${workers}
--- bin/stop-workers.sh
+++ bin/stop-workers.sh
@@ -1,5 +1,8 @@
-rootdir="/opt/buildbot"
-workers="jupiter mars saturn desktop neptune"
+progdir="$(dirname "$0")"
+rootdir="$(cd "${progdir}/.."; pwd)"
+
+workers="$(cd "${rootdir}/master"; python3 -c "from config import workers; print(' '.join(workers))")"
+cmd="/opt/buildbot/bin/start-worker.sh"
 
 cmd="${rootdir}/bin/stop-worker.sh; rm -f ${rootdir}/slave/twistd.pid"
 
