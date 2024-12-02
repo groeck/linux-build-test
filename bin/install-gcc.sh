13d2912216bf51c2b1b5a0617791d27275d01012
Guenter Roeck <linux@roeck-us.net>
2024-12-02 12:50:39 -0800

    Toolchain installation improvements
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/install-gcc.sh
+++ bin/install-gcc.sh
@@ -3,20 +3,35 @@
 TOOLCHAIN_DIR="/opt/toolchains"
 
 if [ -z "$1" ]; then
-    echo "Need parameter"
+    echo "Need file parameter"
     exit 1
 fi
 
-if [ -f /tmp/$1 ]; then
-    f=/tmp/$1
-elif [ -f $1 ]; then
-    f=$1
+tfile="$(echo $1 | sed -e 's/x86_64-//' | sed -e 's/.tar.*//')"
+if [[ "$1" == "${tfile}" ]] || [[ -z "${tfile}" ]]; then
+    echo "$1: Bad tool archive name"
+    exit 1
+fi
+
+if [ -f "/tmp/$1" ]; then
+    f="/tmp/$1"
+elif [ -f "$1" ]; then
+    f="$1"
 else
     echo "$1 does not exist or is not a file"
     exit 1
 fi
 
 if [[ "$(dirname $f)" != "${TOOLCHAIN_DIR}" ]]; then
-    sudo mv $f "${TOOLCHAIN_DIR}"
+    sudo mv "$f" "${TOOLCHAIN_DIR}"
 fi
-sudo tar xf "${TOOLCHAIN_DIR}/$(basename $f)" -C /opt/kernel
+
+basedir="/opt/kernel"
+tdir="${basedir}/${tfile}"
+
+if [[ "$1" != "${tfile}" ]] && [[ -n "${tfile}" ]] && [[ -d "${tdir}" ]]; then
+	echo "Toolchain directory ${tdir} exists and will be removed prior to re-installation."
+	sudo rm -rI "${tdir}"
+fi
+
+sudo tar xf "${TOOLCHAIN_DIR}/$(basename $f)" -C "${basedir}"
--- bin/install-toolchain.sh
+++ bin/install-toolchain.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Need valid parameters"
+if [[ -z "$1" ]] || [[ ! -e "$1" ]]; then
+    echo "Need valid toolchain as parameter"
     exit 1
 fi
 
 doit()
 {
 	echo scp "$2" "$1":
-	scp "$2" "$1":
+	scp "$2" "$1:/tmp"
 	if [ $? -ne 0 ]; then
 		echo "Warning: scp to $1 failed!"
 		# exit 1
 	fi
-	ssh -t $1 /opt/buildbot/bin/install-gcc.sh $(basename $2)
+	ssh -t $1 /opt/buildbot/bin/install-gcc.sh "$(basename $2)"
 }
 
 sys="$(uname -n | cut -f1 -d.)"
@@ -21,9 +21,5 @@ progdir="$(dirname "$0")"
 
 workers="$(cd "${progdir}/../master"; python3 -c "from config import workers; print(' '.join(workers))")"
 for worker in ${workers}; do
-    if [[ "${worker}" == "${sys}" ]]; then
-	sudo tar xf $1 -C /opt/kernel
-    else
-	doit ${worker} $1
-    fi
+    doit ${worker} $1
 done
