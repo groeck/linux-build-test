90d723699c32e58ad95ab8bc302caec6ccd251ff
Guenter Roeck <linux@roeck-us.net>
2025-03-31 13:06:15 -0700

    rootfs:arm64: Skip imx8mp-evk tests for 6.1.y
    Some of the tests fail, others generate warnings.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/arm64/run-qemu-arm64.sh
+++ rootfs/arm64/run-qemu-arm64.sh
@@ -3,6 +3,11 @@
 dir=$(cd $(dirname $0); pwd)
 . "${dir}/run-qemu-arm64-common.sh"
 
+skip_61="imx8mp-evk:defconfig:smp4:mem2G:net=default:initrd \
+	imx8mp-evk:defconfig:smp4:mem2G:sdb2:net=default:ext2 \
+	imx8mp-evk:defconfig:smp4:mem2G:virtio-pci:net=default:ext2 \
+	imx8mp-evk:defconfig:smp4:mem2G:virtio:net=default:ext2"
+
 __runkernel_common ""
 retcode=$?
 
