d189757a2bd6642228873095fc35c60095c95427
Guenter Roeck <linux@roeck-us.net>
2025-04-08 15:10:03 -0700

    rootfs: arm64: Add skip entries for imx8/usb on 6.1
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/arm64/run-qemu-arm64.sh
+++ rootfs/arm64/run-qemu-arm64.sh
@@ -6,7 +6,8 @@ dir=$(cd $(dirname $0); pwd)
 skip_61="imx8mp-evk:defconfig:smp4:mem2G:net=default:initrd \
 	imx8mp-evk:defconfig:smp4:mem2G:sdb2:net=default:ext2 \
 	imx8mp-evk:defconfig:smp4:mem2G:virtio-pci:net=default:ext2 \
-	imx8mp-evk:defconfig:smp4:mem2G:virtio:net=default:ext2"
+	imx8mp-evk:defconfig:smp4:mem2G:virtio:net=default:ext2 \
+	imx8mp-evk:defconfig:smp4:mem2G:usb:net=default:ext2"
 
 __runkernel_common ""
 retcode=$?
