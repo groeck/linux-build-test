6ab7799af09376e12d7f355838232c93b4661f73
Guenter Roeck <linux@roeck-us.net>
2025-06-03 17:08:07 -0700

    master: Add 6.15
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- master/config.py
+++ master/config.py
@@ -1,3 +1,3 @@
 workers = ['server', 'saturn', 'desktop', 'jupiter', 'mars', 'neptune']
 
-releases = ['5.4', '5.10', '5.15', '6.1', '6.6', '6.12', '6.14']
+releases = ['5.4', '5.10', '5.15', '6.1', '6.6', '6.12', '6.14', '6.15']
