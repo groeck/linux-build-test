7959ecdf1a9171a7d2b9acdf525717257ea9825a
Guenter Roeck <linux@roeck-us.net>
2025-06-07 16:40:12 -0700

    rootfs: config: Set default qemu version to 10.0, and update compilers
    Set the default qemu version to 10.0. Update gcc 13.x to gcc 13.4.
    Start supporting gcc 14.x with gcc 14.3. Use binutils 2.44 for all new
    gcc versions.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/scripts/config.sh
+++ rootfs/scripts/config.sh
@@ -10,16 +10,14 @@ QEMU_V92_BIN=${QEMU_INSTALL}/v9.2/bin
 QEMU_V100_BIN=${QEMU_INSTALL}/v10.0/bin
 QEMU_MASTER_BIN=${QEMU_INSTALL}/master/bin
 
-QEMU_BIN=${QEMU_V92_BIN}
+QEMU_BIN=${QEMU_V100_BIN}
 
 # default compiler
 DEFAULT_CC9="gcc-9.5.0-2.37-nolibc"
 DEFAULT_CC11="gcc-11.5.0-2.40-nolibc"
 # gcc 12.4-2.42 fails (assembler errors) for parisc
 DEFAULT_CC12="gcc-12.4.0-2.40-nolibc"
-DEFAULT_CC13="gcc-13.3.0-2.42-nolibc"
-
-# Now set in common.sh
-# DEFAULT_CC="${DEFAULT_CC11}"
+DEFAULT_CC13="gcc-13.4.0-2.44-nolibc"
+DEFAULT_CC14="gcc-14.3.0-2.44-nolibc"
 
 config_initcli="panic=-1"
