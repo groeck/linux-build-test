596ff293cd7e4baba4809c2a6efb4efa3a0bc5b4
Guenter Roeck <linux@roeck-us.net>
2025-06-08 18:41:56 -0700

    rootfs: scripts: Use gcc 13.4 with binutils 2.43
    binutils 2.44 no longer supports nios2 "because it is EOL".
    Use binutils 2.43 instead.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- rootfs/scripts/config.sh
+++ rootfs/scripts/config.sh
@@ -17,7 +17,8 @@ DEFAULT_CC9="gcc-9.5.0-2.37-nolibc"
 DEFAULT_CC11="gcc-11.5.0-2.40-nolibc"
 # gcc 12.4-2.42 fails (assembler errors) for parisc
 DEFAULT_CC12="gcc-12.4.0-2.40-nolibc"
-DEFAULT_CC13="gcc-13.4.0-2.44-nolibc"
+# nios2 is no longer supported in binutils 2.44
+DEFAULT_CC13="gcc-13.4.0-2.43-nolibc"
 DEFAULT_CC14="gcc-14.3.0-2.44-nolibc"
 
 config_initcli="panic=-1"
