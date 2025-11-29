152baeafc0d7f060ad4e4c5028ce0f3fdc75d420
Guenter Roeck <linux@roeck-us.net>
2025-11-29 08:47:56 -0800

    Revert "rootfs: sheb: Revert to gcc 13.4"
    This reverts commit 2c4faca31dc4398345557147f1f84d6e0bf02599.

--- rootfs/sheb/run-qemu-sheb.sh
+++ rootfs/sheb/run-qemu-sheb.sh
@@ -13,8 +13,7 @@ PREFIX=sh4eb-linux-
 ARCH=sh
 DISPARCH=sheb
 
-# Builds with gcc 14.3 die with Linux 6.18-rc1 .
-PATH_SH=/opt/kernel/${DEFAULT_CC13}/sh4eb-linux/bin
+PATH_SH=/opt/kernel/${DEFAULT_CC}/sh4eb-linux/bin
 
 PATH=${PATH_SH}:${PATH}
 
