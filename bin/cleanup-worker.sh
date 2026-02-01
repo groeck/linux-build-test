9aed867fee2a21a2039055fd2dc0b385309c5ad2
Guenter Roeck <linux@roeck-us.net>
2026-01-31 17:29:53 -0800

    cleanup-worker: Use aggressive cleanup
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- bin/cleanup-worker.sh
+++ bin/cleanup-worker.sh
@@ -9,7 +9,7 @@ for x in */build; do
     echo "Cleaning $x:"
     if [ -d "$x" ]; then
 	if [ -e "$x/.git/gc.log" ]; then
-	    git -C "$x" prune
+	    git -C "$x" gc --aggressive
 	    rm -f "$x/.git/gc.log"
 	fi
 	git -C "$x" gc
