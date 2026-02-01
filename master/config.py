09075cb53d5c59fd5d6c6cefd7b922f84b04f20e
Guenter Roeck <linux@roeck-us.net>
2026-01-31 17:33:28 -0800

    buildbot: Drop 6.17
    EOL.
    
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- master/config.py
+++ master/config.py
@@ -1,3 +1,3 @@
 workers = ['server', 'saturn', 'desktop', 'jupiter', 'mars', 'neptune']
 
-releases = ['5.10', '5.15', '6.1', '6.6', '6.12', '6.17', '6.18']
+releases = ['5.10', '5.15', '6.1', '6.6', '6.12', '6.18']
