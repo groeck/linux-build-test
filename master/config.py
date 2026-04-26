2f2d4021e9ac30ae6334193e2855c35b2263c3c0
Guenter Roeck <linux@roeck-us.net>
2026-04-26 11:09:16 -0700

    Add support for 7.0
    Signed-off-by: Guenter Roeck <linux@roeck-us.net>

--- master/config.py
+++ master/config.py
@@ -1,3 +1,3 @@
 workers = ['server', 'saturn', 'desktop', 'jupiter', 'mars', 'neptune']
 
-releases = ['5.10', '5.15', '6.1', '6.6', '6.12', '6.18', '6.19']
+releases = ['5.10', '5.15', '6.1', '6.6', '6.12', '6.18', '6.19', '7.0']
